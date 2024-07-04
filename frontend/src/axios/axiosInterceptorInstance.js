251e2ad4ba0b21386e9c76bcd4d850486869c38d
Nikitimba09 <nickit.schatalow@yandex.ru>
2024-07-04 15:16:21 +0400

    add global handler status

--- frontend/src/axios/axiosInterceptorInstance.js
+++ frontend/src/axios/axiosInterceptorInstance.js
@@ -5,6 +5,8 @@ const axiosInterceptorInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // BASE URL API
 });
 
+const Router = useRouter();
+
 // Request interceptor
 axiosInterceptorInstance.interceptors.request.use(
   (config) => {
@@ -21,24 +23,44 @@ axiosInterceptorInstance.interceptors.request.use(
   }
 );
 
+const handleAuthorizationStatus = (status) => {
+  switch (status) {
+    case 'NEED_CHOOSE_PLAN':
+      Router.push('/choose-plan');
+      break;
+    case 'NEED_CONFIRM_EMAIL':
+      Router.push('/verificate_email');
+      break;
+    case 'FILL_COMPANY_DETAILS':
+      Router.push('/company_details');
+      break;
+    default:
+      break;
+  }
+};
+
 // Response interceptor
 axiosInterceptorInstance.interceptors.response.use(
   (response) => {
+    // Check if the response contains the authorization status
+    if (response.data && response.data.status) {
+      handleAuthorizationStatus(response.data.status);
+    }
     return response;
   },
   (error) => {
     if (error.response) {
       switch (error.response.status) {
         case 401:
-          //401 error handler (Unauthorized)
-          localStorage.clear()
-          useRouter.push('/login')
+          // 401 error handler (Unauthorized)
+          localStorage.clear();
+          Router.push('/login');
           break;
         case 500:
-          //500 error handler (Internal Server Error)
-          useRouter.push("/login");
+          // 500 error handler (Internal Server Error)
+          Router.push('/login');
           break;
-        // TODO: add statuses of other handlers
+        // Handle other statuses if needed
         default:
           console.error("An error occurred:", error.response.data);
       }
