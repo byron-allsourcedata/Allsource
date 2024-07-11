6618080f63994a516ef12b04e106e6c95f796001
donkyH0T <slava.kurenkov@bk.ru>
2024-07-11 15:04:55 +0400

    fix stripe

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s %(levelname)s: %(message)s',
+    ormat='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
