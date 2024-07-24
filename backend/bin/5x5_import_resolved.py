da0560fd6fc14bc52f48196b64c0922fc8774a48
donkyH0T <slava.kurenkov@bk.ru>
2024-07-24 14:13:40 +0400

    fix bugs

--- backend/bin/5x5_import_resolved.py
+++ backend/bin/5x5_import_resolved.py
@@ -1,20 +1,27 @@
 import tempfile
 import time
-import os
 import re
 import boto3
 import pyarrow.parquet as pq
 import logging
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import urllib.parse
 import json
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models.five_x_five_users import FiveXFiveUser
 from models.lead_visits import LeadVisits
 from models.leads import Lead
 from models.leads_users import LeadUser
 from models.users import Users
+from dotenv import load_dotenv
 
+load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 
@@ -36,68 +43,80 @@ def assume_role(role_arn, sts_client):
     return credentials
 
 
+def process_file(bucket, file, session):
+    obj = bucket.Object(file)
+    file_data = obj.get()['Body'].read()
+    if file.endswith('.snappy.parquet'):
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(file_data)
+            temp_file.seek(0)
+            table = pq.read_table(temp_file.name)
+            for i in range(len(table)):
+                up_id = table['UP_ID'][i]
+                if up_id is not None and str(up_id) != 'None':
+                    five_x_five_user = session.query(FiveXFiveUser).filter(
+                        FiveXFiveUser.up_id == str(up_id).lower()).first()
+                    if five_x_five_user:
+                        logging.info(f"UP_ID {up_id} found in table")
+                        lead = Lead(
+                            first_name=five_x_five_user.first_name,
+                            mobile_phone=five_x_five_user.personal_phone,
+                            business_email=five_x_five_user.business_email,
+                            last_name=five_x_five_user.last_name,
+                            up_id=str(up_id),
+                            trovo_id=str(table['TROVO_ID'][i]).lower(),
+                            partner_id=str(table['PARTNER_ID'][i]).lower(),
+                            partner_uid=str(table['PARTNER_UID'][i]).lower(),
+                            sha256_lower_case=str(table['SHA256_LOWER_CASE'][i]).lower(),
+                            ip=str(table['IP'][i]).lower(),
+                            json_headers=str(table['JSON_HEADERS'][i]).lower()
+                        )
+                        session.add(lead)
+                        session.commit()
+                        partner_uid_decoded = urllib.parse.unquote(str(table['PARTNER_UID'][i]).lower())
+                        partner_uid_dict = json.loads(partner_uid_decoded)
+                        partner_uid_client_id = partner_uid_dict.get('client_id')
+                        user = session.query(Users).filter(
+                            Users.data_provider_id == str(partner_uid_client_id)).first()
+                        if user:
+                            lead_id = lead.id
+                            lead_user = LeadUser(lead_id=lead_id, user_id=user.id)
+                            session.add(lead_user)
+                            session.commit()
+                            visited_at = table['EVENT_DATE'][i].as_py().isoformat()
+                            json_data = json.loads(str(table['JSON_HEADERS'][i].as_py()))
+                            referer = json_data.get('Referer', '')
+                            if referer:
+                                referer = referer[0]
+                            lead_visit = LeadVisits(leads_users_id=lead_user.id, visited_at=visited_at,
+                                                    referer=referer)
+                            session.add(lead_visit)
+                            session.commit()
+                            logging.info(f"Processed UP_ID {up_id}")
+        last_processed_file = file
+        logging.info(f"write last processed file {file}")
+        with open("tmp/last_processed_file.txt", "w") as file:
+            file.write(last_processed_file)
+
+
 def process_files(bucket, file_path, last_processed_file, session):
     files = bucket.objects.filter(Prefix=file_path)
+    files_with_dates = []
     for file in files:
-        if last_processed_file and file.key == last_processed_file:
-            continue
-
-        obj = bucket.Object(file.key)
-        file_data = obj.get()['Body'].read()
-
-        if file.key.endswith('.snappy.parquet'):
-            with tempfile.NamedTemporaryFile() as temp_file:
-                temp_file.write(file_data)
-                temp_file.seek(0)
-
-                table = pq.read_table(temp_file.name)
-                for i in range(len(table)):
-                    up_id = table['UP_ID'][i]
-                    if up_id:
-                        five_x_five_user = session.query(FiveXFiveUser).filter(
-                            FiveXFiveUser.up_id == str(up_id).lower()).first()
-                        if five_x_five_user:
-                            logging.info(f"UP_ID {up_id} found in table")
-                            lead = Lead(
-                                first_name=five_x_five_user.first_name,
-                                mobile_phone=five_x_five_user.personal_phone,
-                                business_email=five_x_five_user.business_email,
-                                last_name=five_x_five_user.last_name,
-                                up_id=str(up_id),
-                                trovo_id=str(table['TROVO_ID'][i]).lower(),
-                                partner_id=str(table['PARTNER_ID'][i]).lower(),
-                                partner_uid=str(table['PARTNER_UID'][i]).lower(),
-                                sha256_lower_case=str(table['SHA256_LOWER_CASE'][i]).lower(),
-                                ip=str(table['IP'][i]).lower(),
-                                json_headers=str(table['JSON_HEADERS'][i]).lower()
-                            )
-
-                            session.add(lead)
-                            session.commit()
-                            partner_uid_decoded = urllib.parse.unquote(str(table['PARTNER_UID'][i]).lower())
-                            partner_uid_dict = json.loads(partner_uid_decoded)
-                            partner_uid_client_id = partner_uid_dict.get('client_id')
-                            user = session.query(Users).filter(
-                                Users.data_provider_id == str(partner_uid_client_id)).first()
-                            if user:
-                                lead_id = lead.id
-                                lead_user = LeadUser(lead_id=lead_id, user_id=user.id)
-                                session.add(lead_user)
-                                session.commit()
-                                visited_at = table['EVENT_DATE'][i].as_py().isoformat()
-                                json_data = json.loads(str(table['JSON_HEADERS'][i].as_py()))
-                                referer = json_data.get('Referer', '')
-                                if referer:
-                                    referer = referer[0]
-                                lead_visit = LeadVisits(leads_users_id=lead_user.id, visited_at=visited_at,
-                                                        referer=referer)
-                                session.add(lead_visit)
-                                session.commit()
-
-                                logging.info(f"Processed UP_ID {up_id}")
-
-        last_processed_file = file.key
-
+        file_date = get_date_from_filename(file.key)
+        if file_date:
+            files_with_dates.append((file.key, file_date))
+    sorted_files = sorted(files_with_dates, key=lambda x: x[1])
+    if last_processed_file:
+        file_found = False
+    else:
+        file_found = True
+    for file_key, file_date in sorted_files:
+        if not last_processed_file or file_key.startswith(last_processed_file):
+            if file_found:
+                process_file(bucket, file_key, session)
+            if file_key == last_processed_file:
+                file_found = True
     return last_processed_file
 
 
@@ -109,7 +128,6 @@ def main():
         last_processed_file = None
 
     sts_client = create_sts_client(os.getenv('S3_KEY_ID'), os.getenv('S3_KEY_SECRET'))
-
     credentials = assume_role(os.getenv('S3_ROLE_ARN'), sts_client)
     s3 = boto3.resource('s3', region_name='us-west-2', aws_access_key_id=credentials['AccessKeyId'],
                         aws_secret_access_key=credentials['SecretAccessKey'],
@@ -119,21 +137,17 @@ def main():
     file_path = 'outgoing/cookie_sync/resolved'
     bucket = s3.Bucket(bucket_name)
 
-    engine = create_engine('postgresql://kurenkov_v:ewjd98Ij23oif_ny98$32vei-0HJ@88.99.166.229/maximiz_dev')
+    engine = create_engine(
+        f'postgresql://{os.getenv('POSTGRESQL_LOGIN')}:{os.getenv('POSTGRESQL_PASSWORD')}@{os.getenv('POSTGRESQL_HOST')}/{os.getenv('POSTGRESQL_NAME')}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
     logging.info("Started")
 
     while True:
-        last_processed_file = process_files(bucket, file_path, last_processed_file, session)
-
-        with open("tmp/last_processed_file.txt", "w") as file:
-            file.write(last_processed_file)
-
+        process_files(bucket, file_path, last_processed_file, session)
         logging.info('Sleeping for 10 minutes...')
         time.sleep(60 * 10)
 
 
-if __name__ == "__main__":
-    main()
+main()
