4f968d0746ca1a8f4f35fa3a297b93fe3fbe3d49
donkyH0T <slava.kurenkov@bk.ru>
2024-07-29 16:14:52 +0400

    update 5x5 import resolved, add table location and LeadsLocations

--- backend/bin/5x5_import_resolved.py
+++ backend/bin/5x5_import_resolved.py
@@ -65,87 +65,82 @@ def process_file(bucket, file, session):
                         FiveXFiveUser.up_id == str(up_id).lower()).first()
                     if five_x_five_user:
                         logging.info(f"UP_ID {up_id} found in table")
-                        lead_id = session.query(Lead.id).filter(Lead.up_id == str(up_id).lower()).first()
-                        if lead_id is not None:
-                            lead_id = lead_id[0]
-                        if not lead_id:
-                            age_range = five_x_five_user.age_range
-                            lead = Lead(
-                                first_name=five_x_five_user.first_name,
-                                mobile_phone=five_x_five_user.personal_phone if five_x_five_user.personal_phone is not None else five_x_five_user.mobile_phone,
-                                business_email=five_x_five_user.business_email,
-                                last_name=five_x_five_user.last_name,
-                                up_id=str(up_id),
-                                trovo_id=str(table['TROVO_ID'][i]).lower(),
-                                partner_id=str(table['PARTNER_ID'][i]).lower(),
-                                partner_uid=str(table['PARTNER_UID'][i]).lower(),
-                                sha256_lower_case=str(table['SHA256_LOWER_CASE'][i]).lower(),
-                                ip=str(table['IP'][i]).lower(),
-                                age_min=None,
-                                age_max=None,
-                                gender=five_x_five_user.gender
-                            )
-                            if age_range:
-                                if 'and older' in age_range:
-                                    lead.age_min = lead.age_max = int(age_range.split()[0])
-                                else:
-                                    age_min, age_max = age_range.split('-')
-                                    lead.age_min = int(age_min)
-                                    lead.age_max = int(age_max)
-                            session.add(lead)
-                            session.commit()
-                            lead_id = lead.id
-                        city = five_x_five_user.personal_city
-                        state = five_x_five_user.personal_state
-                        if city:
-                            city = five_x_five_user.personal_city.lower()
-                        if state:
-                            state = five_x_five_user.personal_state.lower()
-                        location = session.query(Locations).filter(Locations.country == 'us',
-                                                                   Locations.city == city,
-                                                                   Locations.state == state).first()
-                        if not location:
-                            location = Locations(
-                                country='us',
-                                city=city,
-                                state=state,
-                            )
-                            session.add(location)
-                            session.commit()
-                        location_id = location.id
-                        existing_leads_locations = session.query(LeadsLocations).filter_by(lead_id=lead_id,
-                                                                                           location_id=location_id).first()
-                        if not existing_leads_locations:
-                            location = LeadsLocations(
-                                lead_id=lead_id,
-                                location_id=location_id,
-                            )
-                            session.add(location)
-                            session.commit()
                         partner_uid_decoded = urllib.parse.unquote(str(table['PARTNER_UID'][i]).lower())
                         partner_uid_dict = json.loads(partner_uid_decoded)
                         partner_uid_client_id = partner_uid_dict.get('client_id')
                         user = session.query(Users).filter(
                             Users.data_provider_id == str(partner_uid_client_id)).first()
                         if user:
-                            lead_user = session.query(LeadUser).filter_by(lead_id=lead_id,
-                                                                          user_id=str(user.id)).first()
-                            if not lead_user:
-                                lead_user = LeadUser(lead_id=lead_id, user_id=user.id)
-                                session.add(lead_user)
+                            lead_id = session.query(Lead.id).filter(Lead.up_id == str(up_id).lower()).first()
+                            if lead_id is not None:
+                                lead_id = lead_id[0]
+                            if not lead_id:
+                                age_range = five_x_five_user.age_range
+                                lead = Lead(
+                                    first_name=five_x_five_user.first_name,
+                                    mobile_phone=five_x_five_user.personal_phone if five_x_five_user.personal_phone is not None else five_x_five_user.mobile_phone,
+                                    business_email=five_x_five_user.business_email,
+                                    last_name=five_x_five_user.last_name,
+                                    up_id=str(up_id),
+                                    trovo_id=str(table['TROVO_ID'][i]).lower(),
+                                    partner_id=str(table['PARTNER_ID'][i]).lower(),
+                                    partner_uid=str(table['PARTNER_UID'][i]).lower(),
+                                    sha256_lower_case=str(table['SHA256_LOWER_CASE'][i]).lower(),
+                                    ip=str(table['IP'][i]).lower(),
+                                    age_min=None,
+                                    age_max=None,
+                                    gender=five_x_five_user.gender
+                                )
+                                if age_range:
+                                    if 'and older' in age_range:
+                                        lead.age_min = lead.age_max = int(age_range.split()[0])
+                                    else:
+                                        age_min, age_max = age_range.split('-')
+                                        lead.age_min = int(age_min)
+                                        lead.age_max = int(age_max)
+                                session.add(lead)
                                 session.commit()
+                                lead_id = lead.id
+                            city = five_x_five_user.personal_city
+                            state = five_x_five_user.personal_state
+                            if city:
+                                city = five_x_five_user.personal_city.lower()
+                            if state:
+                                state = five_x_five_user.personal_state.lower()
+                            location = session.query(Locations).filter(Locations.country == 'us',
+                                                                       Locations.city == city,
+                                                                       Locations.state == state).first()
+                            if not location:
+                                location = Locations(
+                                    country='us',
+                                    city=city,
+                                    state=state,
+                                )
+                                session.add(location)
+                                session.commit()
+                            location_id = location.id
+                            leads_locations = LeadsLocations(
+                                lead_id=lead_id,
+                                location_id=location_id,
+                            )
+                            session.merge(leads_locations)
+                            session.commit()
+                            lead_user = LeadUser(lead_id=lead_id, user_id=user.id)
+                            session.add(lead_user)
+                            session.commit()
                             leads_users_id = lead_user.id
-
-                        visited_at = table['EVENT_DATE'][i].as_py().isoformat()
-                        json_data = json.loads(str(table['JSON_HEADERS'][i].as_py()))
-                        referer = json_data.get('Referer', '')
-                        if referer:
-                            referer = referer[0]
-                        lead_visit = LeadVisits(leads_users_id=leads_users_id, visited_at=visited_at,
-                                                referer=referer)
-                        session.add(lead_visit)
-                        session.commit()
-                        logging.info(f"Processed UP_ID {up_id}")
+                            visited_at = table['EVENT_DATE'][i].as_py().isoformat()
+                            json_data = json.loads(str(table['JSON_HEADERS'][i].as_py()))
+                            referer = json_data.get('Referer', '')
+                            if referer:
+                                referer = referer[0]
+                            lead_visit = LeadVisits(leads_users_id=leads_users_id, visited_at=visited_at,
+                                                    referer=referer)
+                            session.add(lead_visit)
+                            session.commit()
+                            logging.info(f"Processed UP_ID {up_id}")
+                        else:
+                            logging.info(f"User not found with client_id {partner_uid_client_id}")
         last_processed_file = file
         logging.info(f"write last processed file {file}")
         with open("tmp/last_processed_file.txt", "w") as file:
