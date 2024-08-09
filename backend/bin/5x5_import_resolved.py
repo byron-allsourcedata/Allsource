1f2a66f9649d720d172174d3118a9c6bf2fb6738
donkyHOT <slava.kurenkov@bk.ru>
2024-08-09 17:45:45 +0400

    fix 5x5 import resolved in sha256 lc hem

--- backend/bin/5x5_import_resolved.py
+++ backend/bin/5x5_import_resolved.py
@@ -60,9 +60,10 @@ def process_file(bucket, file, session):
                 up_id = table['UP_ID'][i]
                 if not up_id.is_valid and str(up_id) == 'None':
                     up_id = session.query(FiveXFiveHems.up_id).filter(
-                        FiveXFiveHems.sha256_lc_hem == str(table['SHA256_LOWER_CASE'][i])).all()
-                    if up_id is None or len(up_id) != 1:
+                        FiveXFiveHems.sha256_lc_hem == str(table['SHA256_LOWER_CASE'][i])).one_or_none()
+                    if up_id is None:
                         continue
+                    up_id = up_id[0]
                 five_x_five_user = session.query(FiveXFiveUser).filter(
                     FiveXFiveUser.up_id == str(up_id).lower()).first()
                 if five_x_five_user:
