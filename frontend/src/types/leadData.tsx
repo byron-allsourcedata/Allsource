c04509c45d9075db777a533853357ea31db0f19d
donkyHOT <slava.kurenkov@bk.ru>
2024-08-09 13:25:26 +0400

    add types

---
+++ frontend/src/types/leadData.tsx
@@ -0,0 +1,10 @@
+export interface LeadData {
+    id: number;
+    occupation?: string | null;
+    email?: string | null;
+    name?: string;
+    age?: string | null;
+    gender?: string | null;
+    city?: string | null;
+    state?: string | null;
+}
