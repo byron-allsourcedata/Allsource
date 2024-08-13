9625fb816f14daefd11642b91b1570a472e72082
Nikitimba09 <nickit.schatalow@yandex.ru>
2024-08-13 19:25:14 +0400

    pixel perfect BookCall Slider

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect } from 'react';
-import { Drawer, Box, Typography, Button, IconButton, Backdrop } from '@mui/material';
+import { Drawer, Box, Typography, IconButton, Backdrop } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useSlider } from '../context/SliderContext';
 import { PopupButton } from "react-calendly";
@@ -39,54 +39,54 @@ const Slider: React.FC = () => {
         }}
       >
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2, borderBottom: '1px solid #e4e4e4' }}>
-          <Typography variant="h6" sx={{ textAlign: 'center', color: '#4A4A4A', fontFamily: 'Nunito', fontWeight: '600', fontSize: '22px', lineHeight: '25.2px' }}>
+          <Typography variant="h6" sx={{ textAlign: 'center', color: '#4A4A4A', fontFamily: 'Nunito', fontWeight: '600', fontSize: '22px', lineHeight: '25.2px', '@media (max-width: 600px)': { fontSize: '16px' } }}>
             Unlock the full potential with our maximiz!
           </Typography>
           <IconButton onClick={handleClose}>
             <CloseIcon />
           </IconButton>
         </Box>
-        <Box sx={{ pl: 5, pr: 5, display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', justifyContent: 'center' }}>
+        <Box sx={{ pl: 5, pr: 5, display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center', justifyContent: 'center', '@media (max-width: 600px)': { pl: 4, pr: 2 } }}>
           <img src="/slider-bookcall.png" alt="Setup" style={{ width: '50%', marginBottom: '1rem', marginTop: '1em' }} />
-          <div id='calendly-popup-wrapper' className="book-call-button__wrapper">
-            <Typography variant="body1" gutterBottom sx={{ color: '#4A4A4A', textAlign: 'left', fontFamily: 'Nunito', fontWeight: '500', fontSize: '22px', lineHeight: '25.2px', marginBottom: '2em' }}>
+          <div id='calendly-popup-wrapper' className="book-call-button__wrapper"> </div>
+            <Typography variant="body1" gutterBottom sx={{ color: '#4A4A4A', textAlign: 'left', fontFamily: 'Nunito', fontWeight: '500', fontSize: '22px', lineHeight: '25.2px', marginBottom: '2em', '@media (max-width: 600px)': { fontSize: '18px', lineHeight: '22px', marginBottom: '1em'} }}>
               To activate your account, please speak with one of our onboarding specialists, and we&apos;ll get you started.
             </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start' }}>
-              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
-                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)' }} />
-                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', gap: 1}}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5, alignItems: 'center' }}>
+                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)', fontSize: '24px' }} />
+                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', '@media (max-width: 600px)': { fontSize: '16px' } }}>
                   Unlock Optimal Efficiency:
                 </Typography>
               </Box>
-              <Box sx={{ display: 'flex', justifyContent: 'start', pl: 4.5 }}>
-                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '2em' }}>
+              <Box sx={{ display: 'flex', justifyContent: 'start', pl: 4.5, }}>
+                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '2em', '@media (max-width: 600px)': { fontSize: '14px', lineHeight: '18px', marginBottom: '1em' } }}>
                   Maximiz offers advanced tools and features designed to enhance your business performance, driving better outcomes and maximizing your potential.
                 </Typography>
               </Box>
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start' }}>
-              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
-                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)' }} />
-                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', gap: 1}}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5, alignItems: 'center' }}>
+                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)', fontSize: '24px' }} />
+                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', '@media (max-width: 600px)': { fontSize: '16px' } }}>
                   Tailored Expert Guidance:
                 </Typography>
               </Box>
               <Box sx={{ display: 'flex', justifyContent: 'start', pl: 4.5 }}>
-                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '2em' }}>
+                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '2em', '@media (max-width: 600px)': { fontSize: '14px', lineHeight: '18px', marginBottom: '1em' } }}>
                   Our marketing experts are available to provide personalized insights and strategies to help you fully leverage Maximiz's capabilities for your specific needs.
                 </Typography>
               </Box>
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start' }}>
-              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
-                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)' }} />
-                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', gap: 1}}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5, alignItems: 'center' }}>
+                <CheckCircleIcon sx={{ color: 'rgba(110, 193, 37, 1)', fontSize: '24px' }} />
+                <Typography variant="body1" gutterBottom sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', fontWeight: '700', fontSize: '20px', lineHeight: '25.2px', '@media (max-width: 600px)': { fontSize: '16px' } }}>
                   Proven Success in Driving Growth:
                 </Typography>
               </Box>
               <Box sx={{ display: 'flex', justifyContent: 'start', pl: 4.5 }}>
-                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '4em' }}>
+                <Typography sx={{ color: 'rgba(74, 74, 74, 1)', fontFamily: 'Nunito', textAlign: 'left', fontWeight: '400', fontSize: '18px', lineHeight: '19.6px', marginBottom: '4em', '@media (max-width: 600px)': { fontSize: '14px', lineHeight: '18px', marginBottom: '4em' } }}>
                   With Maximiz, you can expect tangible results and significant improvements in your business metrics, backed by expert support every step of the way.
                 </Typography>
               </Box>
@@ -94,8 +94,10 @@ const Slider: React.FC = () => {
             <PopupButton
               className="book-call-button"
               styles={{
+                width: '100%',
+                textWrap: 'nowrap',
                 color: '#fff',
-                padding: '1em 12em',
+                padding: '1em 8em',
                 fontFamily: 'Nunito',
                 fontWeight: '700',
                 fontSize: '16px',
@@ -109,7 +111,6 @@ const Slider: React.FC = () => {
               rootElement={document.getElementById("calendly-popup-wrapper")!}
               text="Get Started"
             />
-          </div>
         </Box>
       </Drawer>
     </>
