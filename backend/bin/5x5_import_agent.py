45fb594268110b7f7df878b6e6effddb11e66695
donkyHOT <slava.kurenkov@bk.ru>
2024-08-14 16:32:59 +0400

    add 5x5 import agent and filler

---
+++ backend/bin/5x5_import_agent.py
@@ -0,0 +1,154 @@
+import asyncio
+import functools
+import gzip
+import json
+import logging
+import os
+import sys
+import tempfile
+
+import aioboto3
+import boto3
+import pandas as pd
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+from dotenv import load_dotenv
+from models.five_x_five_users import FiveXFiveUser
+from config.rmq_connection import RabbitMQConnection
+from models.five_x_five_hems import FiveXFiveHems
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Load environment variables
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+BUCKET_NAME = 'trovo-coop-shakespeare'
+
+
+def create_sts_client(key_id, key_secret):
+    return boto3.client(
+        'sts',
+        aws_access_key_id=key_id,
+        aws_secret_access_key=key_secret,
+        region_name='us-west-2'
+    )
+
+
+def assume_role(role_arn, sts_client):
+    credentials = sts_client.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="create-use-assume-role-scenario"
+    )['Credentials']
+    logging.info(f"Assumed role '{role_arn}', got temporary credentials.")
+    return credentials
+
+
+async def on_message_received(message, s3_session, credentials, db_session):
+    async with message.process():
+        try:
+            message_json = json.loads(message.body)
+            async with s3_session.client(
+                    's3',
+                    region_name='us-west-2',
+                    aws_access_key_id=credentials['AccessKeyId'],
+                    aws_secret_access_key=credentials['SecretAccessKey'],
+                    aws_session_token=credentials['SessionToken']
+            ) as s3:
+                s3_obj = await s3.get_object(Bucket=BUCKET_NAME, Key=message_json['file_name'])
+                async with s3_obj["Body"] as body:
+                    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+                        file_data = await body.read()
+                        temp_file.write(file_data)
+                        temp_file.seek(0)
+                        with gzip.open(temp_file.name, 'rt', encoding='utf-8') as f:
+                            df = pd.read_csv(f)
+                        for _, row in df.iterrows():
+                            last_updated = pd.to_datetime(row.get('LAST_UPDATED', None), unit='s', errors='coerce')
+                            personal_emails_last_seen = pd.to_datetime(row.get('PERSONAL_EMAILS_LAST_SEEN', None),
+                                                                       unit='s', errors='coerce')
+                            company_last_updated = pd.to_datetime(row.get('COMPANY_LAST_UPDATED', None), unit='s',
+                                                                  errors='coerce')
+                            job_title_last_updated = pd.to_datetime(row.get('JOB_TITLE_LAST_UPDATED', None), unit='s',
+                                                                    errors='coerce')
+                            five_x_five_hems = FiveXFiveUser(
+                                up_id=str(row.get('UP_ID', None)),
+                                cc_id=str(row.get('CC_ID', None)),
+                                first_name=str(row.get('FIRST_NAME', None)),
+                                programmatic_business_emails=str(row.get('PROGRAMMATIC_BUSINESS_EMAILS', None)),
+                                mobile_phone=str(row.get('MOBILE_PHONE', None)),
+                                direct_number=str(row.get('DIRECT_NUMBER', None)),
+                                gender=str(row.get('GENDER', None)),
+                                age_range=str(row.get('AGE_RANGE', None)),
+                                personal_phone=str(row.get('PERSONAL_PHONE', None)),
+                                business_email=str(row.get('BUSINESS_EMAIL', None)),
+                                last_name=str(row.get('LAST_NAME', None)),
+                                personal_city=str(row.get('PERSONAL_CITY', None)),
+                                personal_state=str(row.get('PERSONAL_STATE', None)),
+                                company_name=str(row.get('COMPANY_NAME', None)),
+                                company_domain=str(row.get('COMPANY_DOMAIN', None)),
+                                company_phone=str(row.get('COMPANY_PHONE', None)),
+                                company_sic=str(row.get('COMPANY_SIC', None)),
+                                company_address=str(row.get('COMPANY_ADDRESS', None)),
+                                company_city=str(row.get('COMPANY_CITY', None)),
+                                company_state=str(row.get('COMPANY_STATE', None)),
+                                company_zip=str(row.get('COMPANY_ZIP', None)),
+                                company_linkedin_url=str(row.get('COMPANY_LINKEDIN_URL', None)),
+                                company_revenue=str(row.get('COMPANY_REVENUE', None)),
+                                company_employee_count=str(row.get('COMPANY_EMPLOYEE_COUNT', None)),
+                                net_worth=str(row.get('NET_WORTH', None)),
+                                job_title=str(row.get('JOB_TITLE', None)),
+                                sha256_lc_hem=str(row.get('SHA256_LC_HEM', None)),
+                                md5_lc_hem=str(row.get('MD5_LC_HEM', None)),
+                                sha1_lc_hem=str(row.get('SHA1_LC_HEM', None)),
+                                last_updated=None if pd.isna(last_updated) else last_updated,
+                                personal_emails_last_seen=None if pd.isna(
+                                    personal_emails_last_seen) else personal_emails_last_seen,
+                                company_last_updated=None if pd.isna(company_last_updated) else company_last_updated,
+                                job_title_last_updated=None if pd.isna(
+                                    job_title_last_updated) else job_title_last_updated,
+                            )
+                            db_session.merge(five_x_five_hems)
+                        db_session.commit()
+            logging.info(f"{message_json['file_name']} processed")
+        except Exception as e:
+            logging.error(f"Error processing message: {e}", exc_info=True)
+
+
+async def main():
+    logging.info("Started")
+    try:
+        sts_client = create_sts_client(os.getenv('S3_KEY_ID'), os.getenv('S3_KEY_SECRET'))
+        credentials = assume_role(os.getenv('S3_ROLE_ARN'), sts_client)
+
+        rabbitmq_connection = RabbitMQConnection()
+        connection = await rabbitmq_connection.connect()
+        channel = await connection.channel()
+        await channel.set_qos(prefetch_count=1)
+        queue = await channel.declare_queue(
+            name='5x5_import',
+            durable=True,
+        )
+
+        engine = create_engine(
+            f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+        )
+        Session = sessionmaker(bind=engine)
+        db_session = Session()
+
+        session = aioboto3.Session()
+        await queue.consume(
+            functools.partial(on_message_received, s3_session=session, credentials=credentials, db_session=db_session)
+        )
+        await asyncio.Future()
+    except Exception as err:
+        logging.error('Unhandled Exception:', exc_info=True)
+    finally:
+        logging.info('Shutting down...')
+        await rabbitmq_connection.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
