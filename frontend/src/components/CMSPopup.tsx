794765a0cf6c468bd9df744d1d4c3d1d5885721f
Nikitimba09 <nickit.schatalow@yandex.ru>
2024-08-15 16:28:54 +0400

    add Shopify install

--- frontend/src/components/CMSPopup.tsx
+++ frontend/src/components/CMSPopup.tsx
@@ -1,56 +1,104 @@
 "use client";
-import React from 'react';
-import { Box, Button, Typography, Modal, IconButton, Divider } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Typography, Modal, IconButton, Divider, Grid } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import Image from 'next/image';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import axiosInterceptorInstance from '@/axios/axiosInterceptorInstance';
 import styled from 'styled-components';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const StyledOl = styled.ol`
   list-style-type: decimal;
   padding-left: 1.5em;
 `;
+
 const StyledLi = styled.li`
   margin-bottom: 0.5em;
 `;
+
 const StyledLink = styled.a`
   color: blue;
   text-decoration: underline;
 `;
+
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: 'fixed' as 'fixed',
+  top: 0,
+  right: 0,
   width: '40%',
-  height: '90%',
+  height: '95%',
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
   display: 'flex',
   flexDirection: 'column',
+  transition: 'transform 0.3s ease-in-out',
+  transform: 'translateX(100%)',
+  '@media (max-width: 600px)': {
+    width: '100%',
+    height: '100%',
+    p: 0
+  },
 };
-const CenteredImage = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-`;
 
-const StyledImage = styled.img`
-  width: 500px;
-  height: 300px;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
+const openStyle = {
+  transform: 'translateX(0%)',
+  right: 0,
+};
+
+const typographyStyles = {
+  textTransform: 'none',
+  fontFamily: 'Nunito',
+  fontSize: '14px',
+  fontWeight: '500',
+  lineHeight: '19.6px',
+  color: 'rgba(74, 74, 74, 1)',
+  textWrap: 'nowrap',
+  paddingTop: '1em',
+  paddingBottom: '0.75em',
+};
+
+const buttonStyles = {
+  backgroundColor: '#fff',
+  display: "flex",
+  flexDirection: 'column',
+  padding: '1em',
+  borderColor: 'rgba(228, 228, 228, 1)',
+  border: '1px solid rgba(228, 228, 228, 1)',
+  width: '100%',
+};
+
+const buttonGoogle = {
+  backgroundColor: '#fff',
+  display: "flex",
+  flexDirection: 'column',
+  padding: '1em 2em 1.5em 1em',
+  borderColor: 'rgba(228, 228, 228, 1)',
+  border: '1px solid rgba(228, 228, 228, 1)',
+  width: '100%',
+};
+
+const typographyGoogle = {
+  textTransform: 'none',
+  fontFamily: 'Nunito',
+  fontSize: '18px',
+  fontWeight: '450',
+  lineHeight: '19.6px',
+  color: 'rgba(74, 74, 74, 1)',
+  textWrap: 'wrap',
+  paddingTop: '1em',
+  paddingBottom: '0.25em',
+};
 
 const maintext = {
   fontFamily: 'Nunito',
   fontSize: '16px',
   fontWeight: '600',
   lineHeight: '19.6px',
-  textAlign: 'left',
-  color: 'rgba(0, 0, 0, 1)',
-  padding: '0em 0em 0em 1em',
+  color: 'rgba(74, 74, 74, 1)',
+  paddingTop: '1em',
+  paddingBottom: '0.75em',
 };
 
 const subtext = {
@@ -60,8 +108,8 @@ const subtext = {
   lineHeight: '16.8px',
   textAlign: 'left',
   color: 'rgba(0, 0, 0, 1)',
-  paddingTop: '1em',
-  paddingLeft: '2.9em',
+  paddingTop: '0.5em',
+
 };
 
 interface PopupProps {
@@ -71,18 +119,23 @@ interface PopupProps {
 }
 
 const Popup: React.FC<PopupProps> = ({ open, handleClose, pixelCode }) => {
+  const [selectedCMS, setSelectedCMS] = useState<string | null>(null);
+  const [headerTitle, setHeaderTitle] = useState<string>('Install on CMS');
+
   const handleCopy = () => {
     navigator.clipboard.writeText(pixelCode);
     alert('Copied to clipboard');
   };
-const StyledOrderedListItem: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <StyledLi>{children}</StyledLi>;
-};
 
+  const handleButtonClick = async (cms: string) => {
+    setSelectedCMS(cms);
+    setHeaderTitle(`Install with ${cms}`);
+  };
 
-const StyledOrderedList = ({ children }: { children: React.ReactNode }) => {
-  return <StyledOl>{children}</StyledOl>;
-};
+  const handleBackClick = () => {
+    setSelectedCMS(null);
+    setHeaderTitle('Install on CMS');
+  };
 
   return (
     <Modal
@@ -91,66 +144,185 @@ const StyledOrderedList = ({ children }: { children: React.ReactNode }) => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
-        <Box display="flex" justifyContent="space-between" sx={{ width: '100%', paddingBottom: '1em' }}>
-          <Typography variant="h6" component="h2" sx={{ fontFamily: 'Nunito', fontSize: '14px', fontWeight: '700', lineHeight: '19.1px', textAlign: 'left', color: 'rgba(28, 28, 28, 1)' }}>
-            Install on CMS
+      <Box sx={{ ...style, ...(open ? openStyle : {}) }}>
+        <Box display="flex" justifyContent="space-between" sx={{ width: '100%', paddingBottom: '0.5em', alignItems: 'center' }}>
+          <Typography
+            sx={{
+              fontFamily: 'Nunito',
+              fontSize: '14px',
+              fontWeight: '600',
+              lineHeight: '19.6px',
+              textAlign: 'left',
+              color: 'rgba(28, 28, 28, 1)',
+              '@media (max-width: 600px)': { padding: 2 },
+            }}
+          >
+            {headerTitle}
           </Typography>
           <IconButton onClick={handleClose}>
             <CloseIcon />
           </IconButton>
         </Box>
+
         <Divider />
+
         <Box sx={{ flex: 1, overflowY: 'auto', paddingBottom: '4em' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '2em 0em 0em 0em', justifyContent: 'start' }}>
-            <Image src='/1.svg' alt='1' width={28} height={28} />
-            <Typography sx={maintext}>Copy the verification tag</Typography>
-          </Box>
-         <Box
-            component="pre"
-            sx={{
-              backgroundColor: '#ffffff',
-              p: 2,
-              position: 'relative',
-              wordWrap: 'break-word',
-              whiteSpace: 'pre-wrap',
-              border: '1px solid rgba(228, 228, 228, 1)',
-              borderRadius: '10px',
-              marginLeft: '3em',
-              maxHeight: '20em',
-              overflowY: 'auto',
-            }}
-          >
-            <IconButton
-              onClick={handleCopy}
-              sx={{ position: 'absolute', right: '10px', top: '10px' }}
+          {selectedCMS ? (
+            <>
+              <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', }} >
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', gap: 1 }}>
+                  <Button onClick={handleBackClick} sx={{ marginTop: '1em', p: 0}}>
+                    <ArrowBackIcon sx={{color: 'rgba(80, 82, 178, 1)'}} />
+                  </Button>
+                  <Typography sx={{ ...subtext, marginTop: '0.75em' }}>
+                    Follow the instructions to install in Maximiz
+                  </Typography>
+                </Box>
+                {selectedCMS === 'Shopify' ? (
+                  <>
+                    <Box sx={{ flex: 1, overflowY: 'auto', paddingBottom: '2em', pl: 2.25  }}>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1em 0em 0em 0em', justifyContent: 'start' }}>
+                        <Image src='/1.svg' alt='1' width={28} height={28} />
+                        <Typography sx={{ ...maintext, textAlign: 'center', padding: '1em' }}>Copy the pixel code</Typography>
+                      </Box>
+                      <Box
+                        component="pre"
+                        sx={{
+                          backgroundColor: '#ffffff',
+                          p: 2,
+                          position: 'relative',
+                          wordWrap: 'break-word',
+                          whiteSpace: 'pre-wrap',
+                          border: '1px solid rgba(228, 228, 228, 1)',
+                          borderRadius: '10px',
+                          marginLeft: '3em',
+                          maxHeight: '20em',
+                          overflowY: 'auto',
+                          mr: 2
+                        }}
+                      >
+                        <IconButton
+                          onClick={handleCopy}
+                          sx={{ position: 'absolute', right: '10px', top: '10px' }}
+                        >
+                          <ContentCopyIcon />
+                        </IconButton>
+                        <code style={{ color: '#000000' }}>{pixelCode}</code>
+                      </Box>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1em 0em 0em 0em', justifyContent: 'start' }}>
+                        <Image src='/2.svg' alt='2' width={28} height={28} />
+                        <Typography sx={{ ...maintext, textAlign: 'left', padding: '1em' }}>Follow installation guide</Typography>
+                      </Box>
+                      <Button variant="outlined" sx={{ml: 5,backgroundColor: 'rgba(255, 255, 255, 1)', color: 'rgba(80, 82, 178, 1)', textTransform: 'none', padding: '1em 2em', border: '1px solid rgba(80, 82, 178, 1)' }}>
+                        <Typography sx={{fontFamily: 'Nunito', fontSize: '16px', fontWeight: '600', lineHeight: '22.4px'}}>View installation guide</Typography>
+                      </Button>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1em 0em 0em 0em', justifyContent: 'start' }}>
+                        <Image src='/3.svg' alt='3' width={28} height={28} />
+                        <Typography sx={{ ...maintext, textAlign: 'left',  padding: '1em' }}>Verify if Maximiz is receiving data from your site</Typography>
+                      </Box>
+                      <Button variant="outlined" sx={{ml: 5,backgroundColor: 'rgba(255, 255, 255, 1)', color: 'rgba(80, 82, 178, 1)', textTransform: 'none', padding: '1em 2em', border: '1px solid rgba(80, 82, 178, 1)' }}>
+                        <Typography sx={{fontFamily: 'Nunito', fontSize: '16px', fontWeight: '600', lineHeight: '22.4px', textAlign: 'left', textWrap: 'wrap'}}>View installation</Typography>
+                      </Button>
+                    </Box>
+                  </>
+                ) : (
+                  <Typography> WORDPRESS</Typography>
+                )}
+              </Box>
+            </>
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: '2em 0em 0em 0em',
+                justifyContent: 'start',
+                gap: 3,
+                '@media (max-width: 600px)': {
+                  flexDirection: 'column',
+                },
+              }}
             >
-              <ContentCopyIcon />
-            </IconButton>
-            <code style={{ color: '#000000' }}>{pixelCode}</code>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '2em 0em 0em 0em', justifyContent: 'start' }}>
-            <Image src='/2.svg' alt='2' width={28} height={28} />
-            <Typography sx={maintext}>To add the code to your website header or footer, take the following steps:</Typography>
-          </Box>
-          <StyledOrderedList>
-            <StyledOrderedListItem>
-              Install and activate the free <StyledLink href="https://wordpress.com/plugins/insert-headers-and-footers">Insert Headers and Footers plugin</StyledLink> by WPCode.
-            </StyledOrderedListItem>
-            <StyledOrderedListItem>On the left-hand side of your dashboard, navigate to Code Snippets → Header & Footer.</StyledOrderedListItem>
-            <StyledOrderedListItem>Paste the code into the Header or Footer box and click the “Save Changes” button.</StyledOrderedListItem>
-        </StyledOrderedList>
-        <CenteredImage>
-          <StyledImage src="/install_cms3.png" alt="Install on CMS" />
-        </CenteredImage>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', gap: '1em', width: '100%', paddingTop: '1em' }}>
-          <Button variant="outlined" sx={{ backgroundColor: 'rgba(255, 255, 255, 1)', color: 'rgba(80, 82, 178, 1)', textTransform: 'none', padding: '1em 2em', border: '1px solid rgba(80, 82, 178, 1)' }}>
-            Contact support
-          </Button>
-          <Button variant="contained" onClick={handleClose} sx={{ backgroundColor: 'rgba(80, 82, 178, 1)', fontFamily: "Nunito", textTransform: 'none', padding: '1em 3em' }}>
-            Next
-          </Button>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '100%',
+                }}
+              >
+                <Button
+                  variant="outlined"
+                  fullWidth
+                  onClick={() => handleButtonClick('Shopify')}
+                  sx={{
+                    ...buttonGoogle,
+                    '@media (max-width: 600px)': {
+                      width: '90%',
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'start',
+                      gap: 1
+                    },
+                  }}
+                >
+                  <Image
+                    src={'/install_cms1.svg'}
+                    alt="Install on CMS"
+                    width={38}
+                    height={38}
+                    style={{ marginRight: 4 }}
+                  />
+                  <Typography sx={typographyGoogle}>Shopify</Typography>
+                </Button>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '100%',
+                }}
+              >
+                <Button
+                  variant="outlined"
+                  fullWidth
+                  onClick={() => handleButtonClick('WordPress')}
+                  sx={{
+                    ...buttonStyles,
+                    '@media (max-width: 600px)': {
+                      width: '90%',
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'start',
+                      gap: 1
+                    },
+                  }}
+                >
+                  <Image
+                    src={'/install_cms2.svg'}
+                    alt="Install on CMS"
+                    width={38}
+                    height={38}
+                  />
+                  <Typography sx={{ ...typographyStyles, pt: 1.75 }}>
+                    WordPress
+                  </Typography>
+                </Button>
+              </Grid>
+            </Box>
+          )}
         </Box>
       </Box>
     </Modal>
