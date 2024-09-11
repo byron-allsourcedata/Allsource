e7bd78859421578e6f0fa3fd42f0a265a84abc27
yuukich1 <ultimapgg@gmail.com>
2024-09-11 16:46:48 +0400

    AWS

---
+++ backend/services/AWS.py
@@ -0,0 +1,24 @@
+import boto3
+import os
+
+class AWSService:
+
+    def __init__(self) -> None:
+        self.s3_client = boto3.client(
+            's3',
+            aws_access_key_id=os.getenv('S3_KEY_ID'),
+            aws_secret_access_key=os.getenv('S3_KEY_SECRET'),
+            region_name='us-west-2'
+        )
+
+    def upload_string(self, string_data: str, object_name: str) -> None:
+        self.s3_client.put_object(Bucket='maximiz-data', Key=object_name, Body=string_data)
+
+    def download_file(self, bucket_name: str, object_name: str, file_name: str) -> None:
+        self.s3_client.download_file(bucket_name, object_name, file_name)
+
+    def list_files(self, bucket_name: str) -> list:
+        response = self.s3_client.list_objects_v2(Bucket=bucket_name)
+        return [item['Key'] for item in response.get('Contents', [])]
+    
+print(AWSService().list_files('maximiz-data'))
