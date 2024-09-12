fe0fbe07bb667ed70d52c61963d9b1b3f86a301a
Nikitimba09 <nickit.schatalow@yandex.ru>
2024-09-12 19:59:06 +0400

    fix font

--- frontend/src/components/FiltersSlider.tsx
+++ frontend/src/components/FiltersSlider.tsx
@@ -1205,14 +1205,22 @@ const FilterPopup: React.FC<FilterPopupProps> = ({ open, onClose, onApply }) => 
                   </Button>
                 </InputAdornment>
               ),
+              sx: {
+                fontFamily: 'Nunito',
+                fontSize: '0.95em',
+                fontWeight: 400,
+                lineHeight: '19.8px',
+                textAlign: 'left',
+                color: 'rgba(74, 74, 74, 1)',
+              },
             }}
             sx={{
               padding: "1em 1em 0em 1em",
               '& .MuiInputBase-input::placeholder': {
                 fontFamily: 'Nunito',
-                fontSize: '12px',
+                fontSize: '1em',
                 fontWeight: 400,
-                lineHeight: '16.8px',
+                lineHeight: '19.8px',
                 textAlign: 'left',
                 color: 'rgba(74, 74, 74, 1)',
               },
