14ff1a298a9a4d4af3cd6024ed3c1cc0c214bb18
donkyHOT <slava.kurenkov@bk.ru>
2024-09-24 15:53:31 +0400

    change name

--- backend/models/teams_invitations.py
+++ backend/models/teams_invitations.py
@@ -10,12 +10,12 @@ class TeamInvitation(Base):
     __tablename__ = "teams_invitations"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    mail = Column(VARCHAR, nullable=True)
-    access_level = Column(VARCHAR, nullable=True)
+    mail = Column(VARCHAR, nullable=False)
+    access_level = Column(VARCHAR, nullable=False)
     status = Column(VARCHAR, nullable=False)
-    date_invited = Column(TIMESTAMP(precision=7), nullable=True)
-    invited_by_id = Column(BIGINT, nullable=True)
-    team_owner_id = Column(BIGINT, nullable=True)
+    date_invited = Column(TIMESTAMP(precision=7), nullable=False)
+    invited_by_id = Column(BIGINT, nullable=False)
+    team_owner_id = Column(BIGINT, nullable=False)
     token = Column(VARCHAR, nullable=False)
 
 event.listen(TeamInvitation, "before_insert", create_timestamps)
