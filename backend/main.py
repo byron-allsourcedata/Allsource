c9239a33aba70ae5b9f771c795d03ac2b22198ca
donkyHOT <slava.kurenkov@bk.ru>
2024-10-02 14:09:49 +0400

    fix-cors-error

--- backend/main.py
+++ backend/main.py
@@ -59,7 +59,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=Base.allowed_origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allow_headers=["*"]
@@ -67,11 +67,12 @@ app.add_middleware(
 
 external_api.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=Base.allowed_origins,
     allow_credentials=False,
     allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allow_headers=["*"]
 )
 
-app.include_router(main_router)
-app.mount('/external_api', subapi_router)
+app.include_router(subapi_router)
+external_api.include_router(main_router)
+app.mount('/api', external_api)
