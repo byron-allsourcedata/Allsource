dc9d2a87d27bf7cbe61bb7a6e1e0a533bc4ecf81
donkyHOT <slava.kurenkov@bk.ru>
2024-12-11 17:05:39 +0400

    update create_webhooks_for_store

--- backend/services/integrations/shopify.py
+++ backend/services/integrations/shopify.py
@@ -96,12 +96,12 @@ class ShopifyIntegrationService:
         with shopify.Session.temp(shopify_data.shop, ShopifyConfig.api_version, shopify_access_token):
             shopify.Webhook.create({
                 "topic": "app_subscriptions/update",
-                "address": os.getenv("SITE_HOST_URL") + "/api/subscriptions/shopify/billing/webhook",
+                "address": os.getenv("API_SITE_HOST_URL") + "/api/subscriptions/shopify/billing/webhook",
                 "format": "json"
             })
             shopify.Webhook.create({
                 "topic": "app/uninstalled",
-                "address": os.getenv("SITE_HOST_URL") + "/api/integrations/shopify/uninstall",
+                "address": os.getenv("API_SITE_HOST_URL") + "/api/integrations/shopify/uninstall",
                 "format": "json"
             })
 
