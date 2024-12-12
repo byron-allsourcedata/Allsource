cf3c77e1ac413d399e450303b67f6d9c845ff461
donkyHOT <slava.kurenkov@bk.ru>
2024-12-12 16:53:03 +0400

    update bigcommerce code

--- backend/routers/integrations.py
+++ backend/routers/integrations.py
@@ -12,6 +12,8 @@ from schemas.integrations.integrations import *
 from enums import TeamAccessLevel
 from schemas.integrations.shopify import ShopifyLandingResponse, GenericEcommerceResponse
 import httpx
+from bigcommerce.api import BigcommerceApi
+from typing_extensions import Annotated
 from config.bigcommerce import BigcommerceConfig
 
 
@@ -160,10 +162,11 @@ async def bigcommerce_redirect_login(store_hash: str = Query(...), is_pixel_inst
     return {
         'url': authorize_url
     }
-
-@router.get('/bigcommerce/oauth/callback')
-async def bigcommerce_oauth_callback(code: str, state: str = Query(None), integration_service: IntegrationService = Depends(get_integration_service),
-                                     user_persistence: UserPersistence = Depends(get_user_persistence_service), domain_persistence: UserDomainsPersistence = Depends(get_user_domain_persistence)):
+    
+@router.get("/bigcommerce/auth")
+def bigcommerce_auth(code: Optional[str], context: Optional[str], scope: Optional[str], integration_service: IntegrationService = Depends(get_integration_service),
+                                     user_persistence: UserPersistence = Depends(get_user_persistence_service), domain_persistence: UserDomainsPersistence = Depends(get_user_domain_persistence), state: str = Query(None)):
+    state.qwe = 1
     status_oauth = False
     payload = {
         'client_id': BigcommerceConfig.client_id,
@@ -178,6 +181,7 @@ async def bigcommerce_oauth_callback(code: str, state: str = Query(None), integr
             status_oauth = True
             shop_hash = response.json().get('context').split('/')[1]
             access_token = response.json().get('access_token')
+            
     if state:
         parts = state.split(':')
         user_id = parts[0] if len(parts) > 0 else None
@@ -211,6 +215,43 @@ async def bigcommerce_oauth_callback(code: str, state: str = Query(None), integr
                             access_token=access_token
                         )))
             return RedirectResponse(BigcommerceConfig.external_app_installed)
+    
+@router.get("/bigcommerce/uninstall", status_code=status.HTTP_200_OK)
+def oauth_bigcommerce_uninstall(signed_payload: Annotated[str, Query()], signed_payload_jwt: Annotated[str, Query()]):
+    signed_payload.qwe=123
+    # try:
+    #     payload = BigcommerceApi.oauth_verify_payload(signed_payload, BigcommerceConfig.client_secret)
+    #     payload_jwt = BigcommerceApi.oauth_verify_payload_jwt(signed_payload_jwt, BigcommerceConfig.client_secret, BigcommerceConfig.client_id)
+    # except JWTError:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Request [JWT]")
+    # if not payload or not payload_jwt:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Request [NON]")
+    # logger.info(payload)
+    # logger.info(payload_jwt)
+    # bigcommerce_token = db.query(BigcommerceToken).filter(BigcommerceToken.store == payload.get("store_hash")).first()
+    # if bigcommerce_token:
+    #     db.delete(bigcommerce_token)
+    #     db.commit()
+    # return {"status": "OK"}
+
+@router.get("/bigcommerce/load", status_code=status.HTTP_200_OK)
+def oauth_bigcommerce_load(signed_payload: Annotated[str, Query()], signed_payload_jwt: Annotated[str, Query()]):
+    signed_payload.zxc=123
+    # try:
+    #     payload = BigcommerceApi.oauth_verify_payload(signed_payload, BigcommerceConfig.client_secret)
+    #     payload_jwt = BigcommerceApi.oauth_verify_payload_jwt(signed_payload_jwt, BigcommerceConfig.client_secret, BigcommerceConfig.client_id)
+    # except JWTError:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Request [JWT]")
+    # if not payload or not payload_jwt:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Request [NON]")
+    # logger.info(payload)
+    # logger.info(payload_jwt)
+    # bigcommerce_token = db.query(BigcommerceToken).filter(BigcommerceToken.store == payload.get("store_hash")).first()
+    # if bigcommerce_token:
+    #     db.delete(bigcommerce_token)
+    #     db.commit()
+    # return {"status": "OK"}
+    
 
 @router.get('/eai')
 async def get_eais_platform(platform: str = Query(...), integration_service: IntegrationService = Depends(get_integration_service), user = Depends(check_user_authorization)):
