5e4dd608232bb3d81f1b97776b057a5faa61764b
Станислав Забродин <stanislavzabrodin@Stanislavs-Mac-mini.local>
2024-12-13 10:24:05 -0800

    fixed bug

--- backend/routers/partners.py
+++ backend/routers/partners.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
 from dependencies import get_partners_assets_service, check_user_partners_access, PartnersAssetService
 
 router = APIRouter(dependencies=[Depends(check_user_partners_access)])
 
-@router.get('/')
+@router.get('')
 def partners_assets(
                 get_partners_assets_service: PartnersAssetService = Depends(get_partners_assets_service)):
     assets = get_partners_assets_service.get_assets()
