298f7912aa2eaf0d6f373780f3b3aa9330bfabb1
Станислав Забродин <stanislavzabrodin@Stanislavs-Mac-mini.local>
2024-12-13 10:54:53 -0800

    fixed

--- backend/routers/partners.py
+++ backend/routers/partners.py
@@ -3,7 +3,7 @@ from dependencies import get_partners_assets_service, check_user_partners_access
 
 router = APIRouter(dependencies=[Depends(check_user_partners_access)])
 
-@router.get('/')
+@router.get('')
 def partners_assets(
                 get_partners_assets_service: PartnersAssetService = Depends(get_partners_assets_service)):
     assets = get_partners_assets_service.get_assets()
