d3de76accfe0fb7b6c3d2bb24bf0f4d7247e8907
Nikita <95375141+Nikitimba09@users.noreply.github.com>
2025-01-15 15:51:41 +0400

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,14 +5,14 @@ import { useUser } from '@/context/UserContext';
 
 function RedirectPage() {
   const { partner } = useUser();
-  const router = useRouter()
+  const router = useRouter();
   if (typeof window !== 'undefined') {
     const token = localStorage.getItem('token');
       if (token) {
         router.push(partner ? '/partners' : '/dashboard');
       }
       else {
-        router.push('/signin')
+        window.location.href = '/signin'
       }
   }
 }
