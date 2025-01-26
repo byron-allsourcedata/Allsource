12031aaa57f6aa3863542b01bbb7f96785672374
donkyHOT <slava.kurenkov@bk.ru>
2025-01-26 19:18:21 +0400

    update meta service

--- frontend/src/app/(client)/data-sync/components/ConnectMeta.tsx
+++ frontend/src/app/(client)/data-sync/components/ConnectMeta.tsx
@@ -142,7 +142,9 @@ const ConnectMeta: React.FC<ConnectMetaPopupProps> = ({ open, onClose, data }) =
                 service_name: 'meta'
             }
         });
-
+        if (newListResponse.status == 200 && newListResponse.data.url) {
+            window.location.href = newListResponse.data.url;
+        }
         if (newListResponse.status !== 201) {
             throw new Error('Failed to create a new tags')
         }
