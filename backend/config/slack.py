e49ce2e94435854041eee317912552ca80bbf3c2
donkyHOT <slava.kurenkov@bk.ru>
2025-01-27 19:29:13 +0400

    update slack router

--- backend/config/slack.py
+++ backend/config/slack.py
@@ -9,4 +9,4 @@ class SlackConfig(Base):
     redirect_url = f"{os.getenv('API_SITE_HOST_URL')}/api/{os.getenv('SLACK_REDIRECT_URI')}"
     frontend_redirect = f'{os.getenv("SITE_HOST_URL")}/{os.getenv("INTEGRATIONS_PART_URL")}'
     sign_up_redirect = f'{os.getenv("SITE_HOST_URL")}/signup'
-    bot_user_OAuth_token = 'xoxb-7252195355281-8324522205318-oJRRClR3CFto92oK7K7aBo6P'
+    bot_user_OAuth_token = os.getenv("SLACK_OAUTH_TOKEN")
