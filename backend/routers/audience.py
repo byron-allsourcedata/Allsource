ff6ae54dcf8412d1c2a1c40e59c3921438044c8c
donkyHOT <slava.kurenkov@bk.ru>
2025-02-12 14:58:07 +0400

    fix error in audience

--- backend/routers/audience.py
+++ backend/routers/audience.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 
 from dependencies import get_audience_service, check_user_authorization, check_domain
-from schemas.audience import AudienceInfoResponse, AudienceRequest
+from schemas.audience import AudienceRequest
 from typing import List, Optional
 from models.users import User
 from services.audience import AudienceService
@@ -22,8 +22,3 @@ async def post_audience(audience_request: AudienceRequest,
                         audience_service: AudienceService = Depends(get_audience_service)):
     return audience_service.create_audience(user.get('id'), domain.id, audience_request.data_source, audience_request.audience_type, audience_request.audience_threshold)
 
-
-@router.delete("", response_model=AudienceInfoResponse)
-async def delete_audience(audience_request: AudienceRequest,
-                          audience_service: AudienceService = Depends(get_audience_service)):
-    return AudienceInfoResponse(status=audience_service.delete_audience(audience_request.audience_ids))
