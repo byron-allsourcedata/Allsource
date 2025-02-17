f5aadb1af1cd2d17f92f0540f40230e163f21d6a
donkyHOT <slava.kurenkov@bk.ru>
2025-02-17 21:10:53 +0400

    fix model leads_emails_verifications

--- backend/models/leads_emails_verification.py
+++ backend/models/leads_emails_verification.py
@@ -3,10 +3,10 @@ from .base import Base
 
 
 class LeadEmailsVerification(Base):
-    __tablename__ = "leads_emails_verification"
+    __tablename__ = "leads_emails_verifications"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(VARCHAR(64), nullable=False)
+    email = Column(VARCHAR(128), nullable=False)
     is_verify = Column(BOOLEAN, nullable=False, default=False)
     created_at = Column(TIMESTAMP, nullable=False)
     verify_result = Column(VARCHAR(64), nullable=False)
