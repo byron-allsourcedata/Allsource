2ad64b75f10136786a7d17265ede8fbe5dde7bdf
donkyHOT <slava.kurenkov@bk.ru>
2025-02-26 12:44:52 +0400

    fix front bug

--- frontend/src/app/api/upload/route.js
+++ frontend/src/app/api/upload/route.js
@@ -1,38 +1,22 @@
-import fs from "fs";
-import path from "path";
+export async function POST(req, res) {
+  if (req.method === "POST") {
+    const chunks = [];
+    req.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+    req.on("end", () => {
+      const buffer = Buffer.concat(chunks);
+      const filePath = "your/file/path";
 
-export default async function handler(req, res) {
-    if (req.method === "POST") {
-      const chunks = [];
-      req.on("data", (chunk) => {
-        chunks.push(chunk);
-      });
-  
-      req.on("end", () => {
-        const buffer = Buffer.concat(chunks);
-        const uploadsDir = path.join(process.cwd(), "/public/uploads");
-  
-        if (!fs.existsSync(uploadsDir)) {
-          fs.mkdirSync(uploadsDir, { recursive: true });
-        }
-  
-        const filePath = path.join(uploadsDir, `uploaded_file_${Date.now()}.csv`);
-        fs.writeFileSync(filePath, buffer);
-  
-        res.status(200).json({ message: "Файл загружен", filePath });
-      });
-  
-      req.on("error", (err) => {
-        console.error(err);
-        res.status(500).json({ message: "Ошибка загрузки файла" });
-      });
-    } else {
-      res.status(405).json({ message: "Метод не разрешен" });
-    }
+      res.status(200).json({ message: "Файл загружен", filePath });
+    });
+
+    req.on("error", (err) => {
+      console.error(err);
+      res.status(500).json({ message: "Ошибка загрузки файла" });
+    });
+  } else {
+    res.status(405).json({ message: "Метод не разрешен" });
   }
+}
