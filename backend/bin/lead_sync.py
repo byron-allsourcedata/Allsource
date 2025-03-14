a1f69b277d4651234345a12473f47f9665f25da3
donkyHOT <slava.kurenkov@bk.ru>
2025-03-14 10:57:34 +0400

    delet random data

--- backend/bin/lead_sync.py
+++ backend/bin/lead_sync.py
@@ -261,10 +261,7 @@ def generate_random_order_detail():
     }
 
 def process_root_user_behavior(lead_user, behavior_type, requested_at, session):
-    events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    random_event = random.choice(events)
-
-    if behavior_type == 'checkout_completed' or random_event % 4 == 0:
+    if behavior_type == 'checkout_completed':
         if not lead_user.is_converted_sales:
             lead_user.is_converted_sales = True
             session.flush()
@@ -286,7 +283,7 @@ def process_root_user_behavior(lead_user, behavior_type, requested_at, session):
             new_record = LeadsUsersOrdered(lead_user_id=lead_user.id, ordered_at=requested_at)
             session.add(new_record)
 
-    if behavior_type == 'product_added_to_cart' or random_event % 3 == 0:
+    if behavior_type == 'product_added_to_cart':
         existing_record = session.query(LeadsUsersAddedToCart).filter_by(lead_user_id=lead_user.id).first()
         if existing_record:
             existing_record.added_at = requested_at
