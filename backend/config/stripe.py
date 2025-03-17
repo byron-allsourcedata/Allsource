fe17fe9ca186e4874d72f7707572901545425d9e
donkyHOT <slava.kurenkov@bk.ru>
2025-03-17 15:17:23 +0400

    fix config stripe

--- backend/config/stripe.py
+++ backend/config/stripe.py
@@ -6,4 +6,4 @@ from .base import Base
 class StripeConfig(Base):
     api_key = os.getenv("STRIPE_API_KEY")
     success_url = f"{os.getenv('SITE_HOST_URL')}/dashboard"
-    cancel_url = f"{os.getenv('SITE_HOST_URL')}/settings?section=billing"
+    cancel_url = f"{os.getenv('SITE_HOST_URL')}/settings"
