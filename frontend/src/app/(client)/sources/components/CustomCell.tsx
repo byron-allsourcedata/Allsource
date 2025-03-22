e44f81278daa502f5bbe4f63a819df75cb2b24c5
golosoman <consta.lapin2015@gmail.com>
2025-03-22 11:16:28 +0400

    Update table on source page

---
+++ frontend/src/app/(client)/sources/components/CustomCell.tsx
@@ -0,0 +1,142 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { TableCell, Tooltip, Typography, Box, SxProps, Theme } from '@mui/material';
+import { sourcesStyles } from '../sourcesStyles';
+
+interface MyTableCellProps {
+  rowExample: string;
+  cellWidth?: string;
+  loaderForTable?: boolean;
+  customCellStyles?: any;
+}
+
+const createCommonCellStyles = (
+  width: string = "15vw",
+  mobileWidth: string = "20vw"
+) => ({
+  minWidth: width,
+  width: width,
+  maxWidth: width,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  // '@media (max-width: 600px)': {
+  //   minWidth: mobileWidth,
+  //   width: mobileWidth,
+  //   maxWidth: mobileWidth,
+  // },
+});
+
+const CustomCell: React.FC<MyTableCellProps> = ({ rowExample, cellWidth = "16vw", loaderForTable, customCellStyles = {}, }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+  const [isOverflow, setIsOverflow] = useState(false);
+
+  const checkOverflow = useCallback(() => {
+    if (textRef.current) {
+      setIsOverflow(textRef.current.scrollWidth > textRef.current.clientWidth);
+    }
+  }, []);
+
+  useEffect(() => {
+    checkOverflow();
+  }, [rowExample, cellWidth, checkOverflow]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      checkOverflow();
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [checkOverflow]);
+
+  const cellContent = (
+    <Typography
+      ref={textRef}
+      className="table-data"
+      sx={{
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        maxWidth: cellWidth,
+      }}
+    >
+      {rowExample}
+    </Typography>
+  );
+
+  return (
+    <TableCell
+      className="sticky-cell"
+      sx={{
+        position: 'sticky',
+        left: '0',
+        ...createCommonCellStyles(cellWidth),
+        ...sourcesStyles.table_array,
+        ...customCellStyles,
+
+      }}
+    >
+      <Box sx={{ display: 'flex' }}>
+        {isOverflow ? (
+          <Tooltip
+            title={
+              <Box
+                sx={{
+                  backgroundColor: '#fff',
+                  m: 0,
+                  p: 0,
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography
+                  className="table-data"
+                  component="div"
+                  sx={{ fontSize: '12px !important' }}
+                >
+                  {rowExample}
+                </Typography>
+              </Box>
+            }
+            sx={{ marginLeft: '0.5rem !important' }}
+            componentsProps={{
+              tooltip: {
+                sx: {
+                  backgroundColor: '#fff',
+                  color: '#000',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.12)',
+                  border: '0.2px solid rgba(255, 255, 255, 1)',
+                  borderRadius: '4px',
+                  maxHeight: '100%',
+                  maxWidth: '500px',
+                  padding: '11px 10px',
+                  marginLeft: '0.5rem !important',
+                },
+              },
+            }}
+            placement="right"
+          >
+            <Typography className='table-data'
+                sx={{
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    maxWidth: cellWidth,
+                }}
+            >
+                {cellContent}
+            </Typography>
+
+          </Tooltip>
+        ) : (
+          cellContent
+        )}
+      </Box>
+    </TableCell>
+  );
+};
+
+export default CustomCell;
