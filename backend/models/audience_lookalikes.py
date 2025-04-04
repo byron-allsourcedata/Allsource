7ac28a4d079749866299546565e8a7cce8899081
donkyHOT <slava.kurenkov@bk.ru>
2025-04-04 12:39:27 +0400

    add-significant-fields

--- backend/models/audience_lookalikes.py
+++ backend/models/audience_lookalikes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, TIMESTAMP, VARCHAR, ForeignKey, Index, UUID
+from sqlalchemy import Column, Integer, TIMESTAMP, VARCHAR, ForeignKey, JSON, UUID
 from .base import Base
 from models.audience_sources import AudienceSource
 
@@ -15,5 +15,5 @@ class AudienceLookalikes(Base):
     created_by_user_id = Column(Integer, ForeignKey('users.id', ondelete='SET NULL'), nullable=True)
     processed_size = Column(Integer, default=0, nullable=False)
     size = Column(Integer, default=0, nullable=False)
-    source_uuid = Column(UUID(as_uuid=True),
-                         ForeignKey(AudienceSource.id, ondelete='cascade'), nullable=False)
+    source_uuid = Column(UUID(as_uuid=True), ForeignKey(AudienceSource.id, ondelete='cascade'), nullable=False)
+    significant_fields = Column(JSON, nullable=False)
