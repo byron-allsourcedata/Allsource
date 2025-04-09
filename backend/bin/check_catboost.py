96475cb58059f908ac87e6edcc5e6896c422b068
Salavat Nursafin <krowlexing@gmail.com>
2025-04-09 12:23:05 +0400

    add similar audience feature importance extraction

---
+++ backend/bin/check_catboost.py
@@ -0,0 +1,38 @@
+# run from maximiz > `python3 -m backend.bin.check_catboost`
+import asyncio
+import csv
+import os
+import random
+import sys
+from decimal import Decimal
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
+from ..schemas.similar_audiences import AudienceTransaction
+from ..services.similar_audiences import SimilarAudienceService
+
+
+def read_csv_transactions(file_path: str):
+    user_profiles = []
+
+    with open(file_path, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            children = 0 if row['NumberOfChildren'] == '' else int(row['NumberOfChildren'])
+            amount = children * random.randint(0, 5) * 50 + 100
+            user_profiles.append(AudienceTransaction(**row, amount=Decimal(amount)), )
+
+    return user_profiles
+
+async def main():
+    transactions = read_csv_transactions('backend/data/enrichment.csv')
+    service = SimilarAudienceService()
+    scores = service.get_similarity_scores(transactions)
+
+    print(scores)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
