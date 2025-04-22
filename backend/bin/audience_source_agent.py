5c73cb03a6254c9614d304c9a051f030aaab70a4
golosoman <constan.lapin2015@mail.ru>
2025-04-22 22:17:45 +0400

    update imports for audience_source_agent

--- backend/bin/audience_source_agent.py
+++ backend/bin/audience_source_agent.py
@@ -16,13 +16,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from dotenv import load_dotenv
 
-from models.enrichment_personal_profiles import EnrichmentPersonalProfiles
-from schemas.insights import InsightsByCategory, Personal, Financial, Lifestyle, Voter
-
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 sys.path.append(parent_dir)
 
+
+from models.enrichment_personal_profiles import EnrichmentPersonalProfiles
+from schemas.insights import InsightsByCategory, Personal, Financial, Lifestyle, Voter
 from models.five_x_five_emails import FiveXFiveEmails
 from models.five_x_five_users_emails import FiveXFiveUsersEmails
 from models.five_x_five_users import FiveXFiveUser
