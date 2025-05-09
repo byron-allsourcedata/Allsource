8b48e602d1473a867e7390b215b1786e63d74e10
Nikitimba09 <nickit.schatalow@yandex.ru>
2025-05-09 14:35:46 +0400

    fix lookalikes first time screen

--- frontend/src/app/(client)/lookalikes/page.tsx
+++ frontend/src/app/(client)/lookalikes/page.tsx
@@ -919,7 +919,6 @@ const CreateLookalikePage: React.FC = () => {
                 sx={{
                   border: "1px solid rgba(237, 237, 237, 1)",
                   width: "100%",
-                  mt: 3,
                   padding: 3,
                   borderRadius: "6px",
                 }}
