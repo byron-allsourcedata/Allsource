ed00f38ffe67769e8799d873c60fdf55a17a0bfe
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-05-09 04:26:44 -0700

    added hover on cards

--- frontend/src/app/(client)/components/FirstTimeCards.tsx
+++ frontend/src/app/(client)/components/FirstTimeCards.tsx
@@ -28,16 +28,25 @@ type CardData = {
         }}
       >
         {cardData.map((card, index) => (
-          <Card key={index} sx={{ boxShadow: card.isClickable ? "0px 0px 4px rgba(0, 0, 0, 0.3)" : 0, borderColor: card.isClickable ? "rgba(237, 237, 237, 1)" : "inherit" }}>
-            <CardActionArea onClick={card.onClick} sx={{ ":hover": { backgroundColor: "rgba(232, 239, 255, 0.4)" }, display: "flex", height: "100%", pointerEvents: !card.isClickable ? "none" : "inherit" }}>
+          <Card key={index} sx={{ boxShadow: card.isClickable ? "0px 0px 4px rgba(0, 0, 0, 0.3)" : 0, borderColor: card.isClickable ? "rgba(237, 237, 237, 1)" : "inherit",":hover":  { borderColor: "rgba(1, 113, 248, 0.5)" }}}>
+            <CardActionArea onClick={card.onClick} sx={{
+              ":hover": {
+                backgroundColor: "rgba(232, 239, 255, 0.4)",
+                border: "1px solid rgba(1, 113, 248, 0.5)",
+                "& .fiveth-sub-title": { color: "rgba(21, 22, 25, 1)" },
+            },
+              display: "flex", 
+              alignItems: "start", 
+              height: "100%", 
+              pointerEvents: !card.isClickable ? "none" : "inherit" }}>
               <CardContent>
-                <Typography className="fiveth-sub-title" sx={{ marginBottom: 2 }}>
+                <Typography className="fiveth-sub-title" sx={{ marginBottom: 2}}>
                   {card.title}
                 </Typography>
                 <Box
                     sx={{
                       height: 140,
-                      backgroundColor: "#f0f4ff",
+                      backgroundColor: "rgba(232, 239, 255, 1)",
                       backgroundImage: `url(${card.icon})`,
                       backgroundPosition: "center",
                       backgroundRepeat: "no-repeat",
