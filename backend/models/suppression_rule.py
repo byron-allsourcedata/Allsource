8b11808c2c9ce398a9a206525b8e8b937eefa897
donkyHOT <slava.kurenkov@bk.ru>
2025-05-19 09:45:05 +0400

    fix field in suppressions_rules

--- backend/models/suppression_rule.py
+++ backend/models/suppression_rule.py
@@ -1,23 +1,65 @@
-from sqlalchemy import Column, event, Integer, BOOLEAN, TIMESTAMP, TEXT
+from sqlalchemy import Column, event, Integer, BOOLEAN, TIMESTAMP, TEXT, ForeignKey, Boolean, text, Index
 from .base import Base, create_timestamps
 
 
 class SuppressionRule(Base):
-    __tablename__ = "suppressions_rules"
+    __tablename__ = 'suppressions_rules'
+    __table_args__ = (
+        Index('suppressions_rules_domain_id_idx', 'domain_id', unique=True),
+    )
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    created_at = Column(TIMESTAMP, nullable=True)
-    is_stop_collecting_contacts = Column(BOOLEAN, nullable=False, default=False)
-    is_url_certain_activation = Column(BOOLEAN, nullable=False, default=False)
-    activate_certain_urls = Column(TEXT, nullable=True)
-    is_based_activation = Column(BOOLEAN, nullable=False, default=False)
-    is_delete_contacts = Column(BOOLEAN, nullable=False, default=False)
-    activate_based_urls = Column(TEXT, nullable=True)
-    domain_id = Column(Integer, nullable=False)
-    actual_contect_days = Column(Integer, nullable=False)
-    page_views_limit = Column(Integer, nullable=True)
-    collection_timeout = Column(Integer, nullable=True)
-    suppressions_multiple_emails = Column(TEXT, nullable=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        server_default=text("nextval('suppressions_rules_id_seq'::regclass)")
+    )
+    created_at = Column(
+        TIMESTAMP,
+        nullable=True
+    )
+    is_stop_collecting_contacts = Column(
+        Boolean,
+        nullable=True
+    )
+    is_url_certain_activation = Column(
+        Boolean,
+        nullable=True
+    )
+    activate_certain_urls = Column(
+        TEXT,
+        nullable=True
+    )
+    is_based_activation = Column(
+        Boolean,
+        nullable=True
+    )
+    activate_based_urls = Column(
+        TEXT,
+        nullable=True
+    )
+    domain_id = Column(
+        Integer,
+        ForeignKey('users_domains.id', ondelete='CASCADE'),
+        nullable=False
+    )
+    actual_contect_days = Column(
+        Integer,
+        nullable=True
+    )
+    page_views_limit = Column(
+        Integer,
+        nullable=True
+    )
+    collection_timeout = Column(
+        Integer,
+        nullable=True
+    )
+    suppressions_multiple_emails = Column(
+        TEXT,
+        nullable=True
+    )
+    is_delete_contacts = Column(BOOLEAN, nullable=False, server_default=text('FALSE'))
     
     def to_dict(self):
             return {
