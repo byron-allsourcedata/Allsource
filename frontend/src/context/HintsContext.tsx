4cbbd022a0ad4b211f0e9eea52ee5949df22074a
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-05-20 04:57:24 -0700

    universalized storage hints

--- frontend/src/context/HintsContext.tsx
+++ frontend/src/context/HintsContext.tsx
@@ -1,21 +1,53 @@
 import React, { createContext, useState, ReactNode, useContext } from 'react';
 
 interface HintsContextType {
-    showHints: boolean
-    toggleHints: () => void
-  }
+  showHints: boolean
+  toggleHints: () => void
+  sourcesBuilderHints: StateHint[]
+  toggleSourceBuilderHintState: (id: number, action?: boolean) => void
+}
+
+interface StateHint {
+  id: number;
+  show: boolean;
+}
 
 interface HintsProviderProps {
-    children: ReactNode;
-  }
+  children: ReactNode;
+}
 
 const HintsContext = createContext<HintsContextType | undefined>(undefined);
 
 export const HintsProvider: React.FC<HintsProviderProps>  = ({ children }) => {
   const [showHints, setShowHints] = useState(false);
 
+  const [sourcesBuilderHints, setSourcesBuilderHints] = useState<StateHint[]>([
+    { show: true, id: 0 },
+    { show: false, id: 1 },
+    { show: false, id: 2 },
+    { show: false, id: 3 },
+    { show: false, id: 4 },
+    { show: false, id: 5 },
+  ]);
+
+  const toggleSourceBuilderHintState = (id: number, state?: boolean) => {
+    setSourcesBuilderHints((prev) =>
+      prev.map((el) =>
+        el.id === id
+          ? { ...el, show: state !== undefined ? state : !el.show }
+          : el
+      )
+    );
+  };
+
+
   return (
-    <HintsContext.Provider value={{ showHints, toggleHints: () => setShowHints((prev) => !prev) }}>
+    <HintsContext.Provider value={{ 
+      showHints, 
+      toggleHints: () => setShowHints((prev) => !prev),
+      sourcesBuilderHints,
+      toggleSourceBuilderHintState,
+      }}>
       {children}
     </HintsContext.Provider>
   );
