abbd020d9c51bd0f350970ac127e4ab3155bd930
Nikitimba09 <nickit.schatalow@yandex.ru>
2025-05-20 10:46:26 +0400

    update components

--- frontend/src/app/(client)/insights/[type]/[uuid]/components/SemiCircularGradientChart.tsx
+++ frontend/src/app/(client)/insights/[type]/[uuid]/components/SemiCircularGradientChart.tsx
@@ -2,7 +2,7 @@
 
 import React, { useId } from "react";
 import { Box, Typography } from "@mui/material";
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 type ColorStop = {
   offset: string;
@@ -21,7 +21,6 @@ type SemiCircularGradientChartProps = {
 export const SemiCircularGradientChart: React.FC<
   SemiCircularGradientChartProps
 > = ({ title, percent, labelLeft, labelRight, colorStops, rank }) => {
-
   const radius = 90;
   const strokeWidth = 20;
   const center = radius + strokeWidth / 2;
@@ -35,80 +34,78 @@ export const SemiCircularGradientChart: React.FC<
         width: "100%",
         borderRadius: "6px",
         boxShadow: "0px 1px 4px 0px rgba(0, 0, 0, 0.25)",
-        position: 'relative',
-        flexGrow: 1
+        position: "relative",
+        flexGrow: 1,
       }}
     >
       {rank !== undefined && (
         <Box
           sx={{
-            position: 'absolute',
+            position: "absolute",
             top: 0,
             right: 0,
-            display: 'inline-flex',
-            alignItems: 'center',
-            backgroundColor: 'rgba(30, 136, 229, 1)',
-            color: 'white',
-            borderTopRightRadius: '4px',
-            borderBottomLeftRadius: '4px',
+            display: "inline-flex",
+            alignItems: "center",
+            backgroundColor: "rgba(30, 136, 229, 1)",
+            color: "white",
+            borderTopRightRadius: "4px",
+            borderBottomLeftRadius: "4px",
             px: 1.5,
             py: 0.5,
-            fontFamily: 'Roboto',
+            fontFamily: "Roboto",
             fontSize: 12,
             fontWeight: 500,
-            maxWidth: '100%',
-            whiteSpace: 'nowrap',
+            maxWidth: "100%",
+            whiteSpace: "nowrap",
             ...(rank !== undefined && {
-              overflow: 'hidden',
-              '&::before, &::after': {
+              overflow: "hidden",
+              "&::before, &::after": {
                 content: '""',
-                position: 'absolute',
-                backgroundRepeat: 'no-repeat',
-                pointerEvents: 'none',
+                position: "absolute",
+                backgroundRepeat: "no-repeat",
+                pointerEvents: "none",
               },
-              '&::before': {
+              "&::before": {
                 top: 0,
                 right: 0,
-                height: '1px',
-                width: '100%',
+                height: "1px",
+                width: "100%",
                 backgroundImage:
-                  'linear-gradient(to left, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)',
+                  "linear-gradient(to left, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)",
               },
-              '&::after': {
+              "&::after": {
                 top: 0,
                 right: 0,
-                width: '1px',
-                height: '100%',
+                width: "1px",
+                height: "100%",
                 backgroundImage:
-                  'linear-gradient(to bottom, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)',
+                  "linear-gradient(to bottom, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)",
               },
             }),
           }}
         >
-          <ArrowDropUpIcon sx={{ fontSize: 16, mr: 0.5 }} />
-          #{rank} Predictable field
+          <ArrowDropUpIcon sx={{ fontSize: 16, mr: 0.5 }} />#{rank} Predictable
+          field
         </Box>
       )}
       <Box
         bgcolor="#fff"
         p={2}
         sx={{
-          width: "100%"
-        }}>
-
+          width: "100%",
+        }}
+      >
         <Box
           sx={{
-            width: '100%',
-            justifyContent: 'space-between',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
+            width: "100%",
+            justifyContent: "space-between",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
             mb: 2,
           }}
         >
-          <Typography className="dashboard-card-heading" >
-            {title}
-          </Typography>
+          <Typography className="dashboard-card-heading">{title}</Typography>
         </Box>
 
         <Box display="flex" justifyContent="center" alignItems="end">
@@ -125,7 +122,11 @@ export const SemiCircularGradientChart: React.FC<
             <defs>
               <linearGradient id={gradientId} gradientTransform="rotate(90)">
                 {colorStops.map((stop, index) => (
-                  <stop key={index} offset={stop.offset} stopColor={stop.color} />
+                  <stop
+                    key={index}
+                    offset={stop.offset}
+                    stopColor={stop.color}
+                  />
                 ))}
               </linearGradient>
             </defs>
@@ -151,7 +152,9 @@ export const SemiCircularGradientChart: React.FC<
             <Typography className="dashboard-card-text">
               {(100 - percent).toFixed(2)}%
             </Typography>
-            <Typography className="dashboard-card-text">{labelRight}</Typography>
+            <Typography className="dashboard-card-text">
+              {labelRight}
+            </Typography>
           </Box>
         </Box>
       </Box>
--- frontend/src/app/(client)/insights/[type]/[uuid]/components/VerticalGradientBarChart.tsx
+++ frontend/src/app/(client)/insights/[type]/[uuid]/components/VerticalGradientBarChart.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Box, Typography, Stack } from "@mui/material";
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 export type BarData = {
   label: string;
@@ -31,7 +31,6 @@ const getGradient = (relativePercent: number, gradientColor: string) => {
 export const VerticalGradientBarChart: React.FC<
   VerticalGradientBarChartProps
 > = ({ title, data, rank, gradientColor = "155, 223, 196" }) => {
-
   const maxPercent = Math.max(...data.map((d) => d.percent)) || 1;
 
   return (
@@ -41,59 +40,59 @@ export const VerticalGradientBarChart: React.FC<
         width: "100%",
         borderRadius: "6px",
         boxShadow: "0px 1px 4px 0px rgba(0, 0, 0, 0.25)",
-        position: 'relative',
-        height: '100%',
-        flexGrow: 1
+        position: "relative",
+        height: "100%",
+        flexGrow: 1,
       }}
     >
       {rank !== undefined && (
         <Box
           sx={{
-            position: 'absolute',
+            position: "absolute",
             top: 0,
             right: 0,
-            display: 'inline-flex',
-            alignItems: 'center',
-            backgroundColor: 'rgba(30, 136, 229, 1)',
-            color: 'white',
-            borderTopRightRadius: '4px',
-            borderBottomLeftRadius: '4px',
+            display: "inline-flex",
+            alignItems: "center",
+            backgroundColor: "rgba(30, 136, 229, 1)",
+            color: "white",
+            borderTopRightRadius: "4px",
+            borderBottomLeftRadius: "4px",
             px: 1.5,
             py: 0.5,
-            fontFamily: 'Roboto',
+            fontFamily: "Roboto",
             fontSize: 12,
             fontWeight: 500,
-            maxWidth: '100%',
-            whiteSpace: 'nowrap',
+            maxWidth: "100%",
+            whiteSpace: "nowrap",
             ...(rank !== undefined && {
-              overflow: 'hidden',
-              '&::before, &::after': {
+              overflow: "hidden",
+              "&::before, &::after": {
                 content: '""',
-                position: 'absolute',
-                backgroundRepeat: 'no-repeat',
-                pointerEvents: 'none',
+                position: "absolute",
+                backgroundRepeat: "no-repeat",
+                pointerEvents: "none",
               },
-              '&::before': {
+              "&::before": {
                 top: 0,
                 right: 0,
-                height: '1px',
-                width: '100%',
+                height: "1px",
+                width: "100%",
                 backgroundImage:
-                  'linear-gradient(to left, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)',
+                  "linear-gradient(to left, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)",
               },
-              '&::after': {
+              "&::after": {
                 top: 0,
                 right: 0,
-                width: '1px',
-                height: '100%',
+                width: "1px",
+                height: "100%",
                 backgroundImage:
-                  'linear-gradient(to bottom, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)',
+                  "linear-gradient(to bottom, rgba(30,136,229,1) 0%, rgba(30,136,229,0) 100%)",
               },
             }),
           }}
         >
-          <ArrowDropUpIcon sx={{ fontSize: 16, mr: 0.5 }} />
-          #{rank} Predictable field
+          <ArrowDropUpIcon sx={{ fontSize: 16, mr: 0.5 }} />#{rank} Predictable
+          field
         </Box>
       )}
       <Box
@@ -101,90 +100,100 @@ export const VerticalGradientBarChart: React.FC<
         p={2}
         sx={{
           width: "100%",
-          display: 'flex',
-          height: '100%',
-          flexDirection: 'column',
-          gap: 2
-        }}>
-
+          display: "flex",
+          height: "100%",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
         <Box
           sx={{
-            width: '100%',
-            justifyContent: 'space-between',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
+            width: "100%",
+            justifyContent: "space-between",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
             mb: 2,
           }}
         >
-          <Typography className="dashboard-card-heading" >
-            {title}
-          </Typography>
+          <Typography className="dashboard-card-heading">{title}</Typography>
         </Box>
 
-        <Stack
-          direction="row"
-          justifyContent="space-around"
-          alignItems="start"
+        {data.length === 0 ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="120px"
+          >
+            <Typography className="second-sub-title">
+              No data available
+            </Typography>
+          </Box>
+        ) : (
+          <Stack
+            direction="row"
+            justifyContent="space-around"
+            alignItems="start"
+            spacing={0}
+          >
+            {data.map(({ label, percent }, index) => {
+              const relative = percent / maxPercent;
 
-          spacing={0}
-        >
-          {data.map(({ label, percent }, index) => {
-            const relative = percent / maxPercent;
-
-            return (
-              <Box
-                key={index}
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                width={41}
-              >
-                {/* Bar container */}
+              return (
                 <Box
-                  height={240}
-                  width="100%"
-                  borderRadius={5}
-                  bgcolor="rgba(245, 250, 254, 1)"
+                  key={index}
                   display="flex"
-                  alignItems="flex-end"
-                  position="relative"
-                  overflow="hidden"
+                  flexDirection="column"
+                  alignItems="center"
+                  width={41}
                 >
-                  {/* Filled part */}
+                  {/* Bar container */}
                   <Box
+                    height={240}
                     width="100%"
-                    height={`${percent}%`}
-                    sx={{
-                      backgroundColor: getGradient(relative, gradientColor),
-                      transition: "height 0.3s ease, background 0.3s ease",
-                    }}
-                  />
-                </Box>
+                    borderRadius={5}
+                    bgcolor="rgba(245, 250, 254, 1)"
+                    display="flex"
+                    alignItems="flex-end"
+                    position="relative"
+                    overflow="hidden"
+                  >
+                    {/* Filled part */}
+                    <Box
+                      width="100%"
+                      height={`${percent}%`}
+                      sx={{
+                        backgroundColor: getGradient(relative, gradientColor),
+                        transition: "height 0.3s ease, background 0.3s ease",
+                      }}
+                    />
+                  </Box>
 
-                {/* Bottom label (param name) */}
-                <Typography
-                  className="dashboard-gist-card-text"
-                  sx={{
-                    marginTop: 0.5,
-                    textAlign: "center",
-                    fontSize: 12,
-                    textWrap: "wrap",
-                  }}
-                >
-                  {label}
-                </Typography>
-                {/* Top label (percent) */}
-                <Typography
-                  className="dashboard-gist-card-text"
-                  sx={{ marginBottom: 0.5 }}
-                >
-                  {percent}%
-                </Typography>
-              </Box>
-            );
-          })}
-        </Stack>
+                  {/* Bottom label (param name) */}
+                  <Typography
+                    className="dashboard-gist-card-text"
+                    sx={{
+                      marginTop: 0.5,
+                      textAlign: "center",
+                      fontSize: 12,
+                      textWrap: "wrap",
+                    }}
+                  >
+                    {label}
+                  </Typography>
+                  {/* Top label (percent) */}
+                  <Typography
+                    className="dashboard-gist-card-text"
+                    sx={{ marginBottom: 0.5 }}
+                  >
+                    {percent}%
+                  </Typography>
+                </Box>
+              );
+            })}
+          </Stack>
+        )}
       </Box>
     </Box>
   );
