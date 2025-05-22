810cb9dd058f8e797ea9d208c40dde1913440e2c
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-05-21 23:09:01 -0700

    update pulsing dot animation

--- frontend/src/app/(client)/components/PulsingDot.tsx
+++ frontend/src/app/(client)/components/PulsingDot.tsx
@@ -7,24 +7,66 @@ interface PulsingDotComponentProps {
     rightSide?: boolean
   }
 
+// const ripple = keyframes`
+//   0% {
+//     transform: scale(1);
+//     opacity: 1;
+//   }
+//   100% {
+//     transform: scale(3);
+//     opacity: 0;
+//   }
+// `;
+
+// const PulsingDot = styled(Box)(() => ({
+//   width: "16px",
+//   height: "16px",
+//   backgroundColor: "#3898FC",
+//   borderRadius: "50%",
+//   animation: `${ripple} 1.5s infinite ease-in-out`,
+// }));
+
+
 const ripple = keyframes`
   0% {
     transform: scale(1);
     opacity: 1;
   }
   100% {
-    transform: scale(3);
+    transform: scale(4);
     opacity: 0;
   }
 `;
 
-const PulsingDot = styled(Box)(() => ({
-  width: "16px",
-  height: "16px",
-  backgroundColor: "#3898FC",
-  borderRadius: "50%",
-  animation: `${ripple} 1.5s infinite ease-in-out`,
-}));
+const PulsingDot: React.FC<React.ComponentProps<typeof Box>> = (props) => {
+  return (
+    <Box
+      {...props}
+      sx={{
+        position: "relative",
+        width: "16px",
+        height: "16px",
+        backgroundColor: "rgba(56, 152, 252, 1)",
+        borderRadius: "50%",
+        "&::before, &::after": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          borderRadius: "50%",
+          background: "linear-gradient(rgba(56, 152, 252, 0), rgba(56, 152, 252, 1))",
+          boxShadow: "rgba(56, 152, 252, 1)",
+          animation: `${ripple} 6s ease-out infinite`,
+        },
+        "&::after": {
+          animationDelay: "2s",
+        },
+      }}
+    />
+  );
+};
 
 
 const PulsingDotComponent: React.FC<PulsingDotComponentProps> = ({ toggleClick, rightSide }) => {
