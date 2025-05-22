100bed18d2a8654e918aff353445be2fe2eae084
donkyHOT <slava.kurenkov@bk.ru>
2025-05-22 18:19:36 +0400

    add router clean-account

--- backend/external_api_routers/__init__.py
+++ backend/external_api_routers/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from external_api_routers import pixel_installation
+from . import clean_account
 
 subapi_router = APIRouter(prefix='/external_api')
 
 subapi_router.include_router(pixel_installation.router, prefix='/install-pixel')
+
+subapi_router.include_router(clean_account.router, prefix='/clean-account')
