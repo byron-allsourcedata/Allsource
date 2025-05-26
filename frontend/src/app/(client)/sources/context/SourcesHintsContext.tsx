6b94f83c9267fddd99ebd0ea833d527f86524cd8
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-05-25 23:51:19 -0700

    moved repeat methods from hint context to utils

--- frontend/src/app/(client)/sources/context/SourcesHintsContext.tsx
+++ frontend/src/app/(client)/sources/context/SourcesHintsContext.tsx
@@ -1,22 +1,17 @@
 import React, { createContext, useState, ReactNode, useContext } from 'react';
 import { initialSourcesBuilderHints, initialSourcesTableHints, initialCreatedSourceHints } from './hintsInitialState';
+import { changeHintState, resetHintsState, StateHint, HintKey, HintAction } from '@/utils/hintsUtils';
 
 interface HintsContextType {
-  changeSourcesBuilderHint: (id: number, key: "show" | "showBody", action: "toggle" | "close" | "open") => void
-  changeSourcesTableHint: (id: number, key: "show" | "showBody", action: "toggle" | "close" | "open") => void
-  changeCreatedSourceHint: (id: number, key: "show" | "showBody", action: "toggle" | "close" | "open") => void
+  changeSourcesBuilderHint: (id: number, key: HintKey, action: HintAction) => void;
+  changeSourcesTableHint: (id: number, key: HintKey, action: HintAction) => void;
+  changeCreatedSourceHint: (id: number, key: HintKey, action: HintAction) => void;
   sourcesBuilderHints: StateHint[]
   sourcesTableHints: StateHint[]
   createdSourceHints: StateHint[]
   resetSourcesBuilderHints: () => void
   resetSourcesTableHints: () => void
   resetCreatedSourceHints: () => void
-}
-
-interface StateHint {
-  id: number;
-  show: boolean;
-  showBody?: boolean;
 }
 
 interface HintsProviderProps {
@@ -29,35 +24,6 @@ export const SourcesHintsProvider: React.FC<HintsProviderProps>  = ({ children }
   const [sourcesBuilderHints, setSourcesBuilderHints] = useState<StateHint[]>(initialSourcesBuilderHints);
   const [sourcesTableHints, setSourcesTableHints] = useState<StateHint[]>(initialSourcesTableHints);
   const [createdSourceHints, setCreatedSourceHints] = useState<StateHint[]>(initialCreatedSourceHints);
-
-  const actionMap = {
-    toggle: (currentState: boolean) => !currentState,
-    open: () => true,
-    close: () => false
-  };
-
-  const changeHintState = (
-    id: number,
-    key: "show" | "showBody",
-    action: "toggle" | "close" | "open",
-    setStateFunction: React.Dispatch<React.SetStateAction<StateHint[]>>
-  ) => {
-    setStateFunction((prev) =>
-      prev.map((hint) => {
-        return hint.id === id
-          ? { ...hint, [key]: actionMap[action](hint[key] as boolean) }
-          : hint
-        }
-      )
-    );
-  };
-
-  const resetHintsState = (
-    setStateFunction: React.Dispatch<React.SetStateAction<StateHint[]>>,
-    initialState: StateHint[]
-  ) => {
-    setStateFunction(initialState);
-  };
 
   return (
     <SourcesHintsContext.Provider value={{ 
