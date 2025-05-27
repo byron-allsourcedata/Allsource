8edb341d9b36e6b7937abed0a6205055887fee3e
donkyHOT <slava.kurenkov@bk.ru>
2025-05-27 19:13:18 +0400

    fix url zoho

--- frontend/src/app/(client)/dashboard/components/CMSPopup.tsx
+++ frontend/src/app/(client)/dashboard/components/CMSPopup.tsx
@@ -359,50 +359,50 @@ const Popup: React.FC<PopupProps> = ({ open, pixelCode, pixel_client_id }) => {
         "Enter your Shopify store domain in the provided field. We've prefilled it based on your earlier selection, but you can choose a different one if needed. Note: if you change the domain here, make sure to also update it in the domain selection step.",
       title: "Enter Shop Domain",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify#_Pre-Integration_Requirements",
     },
     {
       description:
         "Enter your Shopify API access token. This token is required for secure communication between your store and our application. You can get the token in your Shopify admin under “Settings” → “Apps and sales channels” → “Develop app” → “Admin API”.",
       title: "Enter a Shopify Access Token",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify#Step_4_Insert_this_token_in_the_access_token_field",
     },
     {
       description: `Click the "Install" button, and we’ll automatically inject our script into your Shopify store. No further action is needed — the setup completes automatically.`,
       title: "Install the Script",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-shopify#Step_4_Insert_this_token_in_the_access_token_field",
     },
     {
       description: `Add our official Allsource Pixel plugin to your WordPress site. This allows for seamless pixel integration without manual setup.`,
       title: "Install the Plugin",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress#Method_1_Install_by_Downloading_the_Plugin",
     },
     {
       description: `Enter your Site ID during the checkout process. This connects your site to Allsource for accurate event tracking.`,
       title: "Enter Your Site ID",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress#WordPress_Integration_Guide",
     },
     {
       description: `Check if Allsource is receiving data from your site. If everything is set up correctly, events will start appearing automatically.`,
       title: "Verify Connection",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/verify-pixel",
     },
     {
       description: `Enter your unique BigCommerce Store Hash in the designated field. This helps our system identify your store. You can find the Store Hash in your admin panel URL — it's the part between /stores/ and /manage.`,
       title: "Enter Store Hash",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-bigcommerce#Step_1_Enter_Your_BigCommerce_Store_Hash",
     },
     {
       description: `Once you’ve submitted the required information, click “Install”. We’ll automatically add our script to your BigCommerce store. No further action is needed on your part.`,
       title: "Script Installation",
       linkToLoadMore:
-        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-wordpress",
+        "https://allsourceio.zohodesk.com/portal/en/kb/articles/how-to-integrate-bigcommerce#Step_2_Install_the_Script_Automatically",
     },
   ];
 
