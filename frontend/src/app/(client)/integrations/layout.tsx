505e1f36c901cabd408a9ca7ad4e90936c9f126b
golosoman <constan.lapin2015@mail.ru>
2025-05-28 20:02:30 +0400

    added data-sync layout to integration layout

--- frontend/src/app/(client)/integrations/layout.tsx
+++ frontend/src/app/(client)/integrations/layout.tsx
@@ -1,13 +1,16 @@
 "use client";
+import { DataSyncHintsProvider } from "../data-sync/context/dataSyncHintsContext";
 import { IntegrationHintsProvider } from "./context/IntegrationsHintsContext";
 
 
-export default function Layout({children}: {
+export default function Layout({ children }: {
   children: React.ReactNode;
 }) {
   return (
-    <IntegrationHintsProvider>
+    <DataSyncHintsProvider>
+      <IntegrationHintsProvider>
         {children}
-    </IntegrationHintsProvider>
+      </IntegrationHintsProvider>
+    </DataSyncHintsProvider>
   );
 }
