cb2bb7b5fcb3e1a94b6275b4fb8499f5e81b625d
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-06-02 07:04:17 -0700

    auto close hints

--- frontend/src/app/(client)/components/HintCard.tsx
+++ frontend/src/app/(client)/components/HintCard.tsx
@@ -37,20 +37,62 @@ const HintCard: React.FC<HintCardProps> = ({
   sx,
 }) => {
   const [showHint, setShowHint] = useState(false);
+  const { showHints } = useHints();
+  const [hintTimeout, setHintTimeout] = useState<NodeJS.Timeout | null>(null);
+  const [isFirstOpen, setIsFirstOpen] = useState(false);
+  const [isSecondOpen, setIsSecondOpen] = useState(false);
+
+
+  // const showBody = () => {
+  //   setShowHint(true);
+  //   const closeTimeout = setTimeout(hideBody, 3000);
+  //   return () => clearTimeout(closeTimeout);
+  // };
+
+  // const hideBody = () => {
+  //   setShowHint(false)
+  // }
+
+  // useTimeout(showBody, 2000);
 
   const showBody = () => {
+    if (hintTimeout) {
+      clearTimeout(hintTimeout);
+    }
+
     setShowHint(true);
+
+    const closeTimeout = setTimeout(() => {
+      setShowHint(false);
+    }, 3000);
+
+    setHintTimeout(closeTimeout);
   };
 
-  const hideBody = () => {
-    setShowHint(true);
-  };
+  React.useEffect(() => {
+    if(isFirstOpen && isSecondOpen){
+      showBody();
+    }
+    else { 
+      const openTimeout = setTimeout(() => {
+        showBody();
+      }, 2000);
 
-  useTimeout(showBody, 2000);
+      return () => {
+        clearTimeout(openTimeout);
+        setIsFirstOpen(true);
+        if (hintTimeout) {
+          clearTimeout(hintTimeout);
+        }
+      };
+    }
 
-  useTimeout(hideBody, 5000);
+    if (!isSecondOpen){
+      setIsSecondOpen(true);
+    }
 
-  const { showHints } = useHints();
+  }, [toggleClick]);
+
 
   return (
     <>
@@ -71,7 +113,7 @@ const HintCard: React.FC<HintCardProps> = ({
               left: positionLeft,
               top: positionTop ?? 10,
               width: 400,
-              pointerEvents: isOpenBody? undefined: "none",
+              pointerEvents: isOpenBody ? undefined : "none",
               ...sx,
             }}
           >
@@ -131,7 +173,9 @@ const HintCard: React.FC<HintCardProps> = ({
               </Box>
             )}
             <PulsingDotComponent
-              toggleClick={toggleClick}
+              toggleClick={() => {
+                toggleClick()
+              }}
               rightSide={rightSide}
             />
           </Box>
