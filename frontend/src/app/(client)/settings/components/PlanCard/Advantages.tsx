0a39a70391737265901b6273cad851b86f0dfbe6
Nikitimba09 <nickit.schatalow@yandex.ru>
2025-06-06 18:06:42 +0400

    fix build

--- frontend/src/app/(client)/settings/components/PlanCard/Advantages.tsx
+++ frontend/src/app/(client)/settings/components/PlanCard/Advantages.tsx
@@ -15,8 +15,8 @@ export type Advantage = {
 export const Advantages: FC<AdvantagesProps> = (props) => {
     const { advantages } = props;
 
-    return advantages.map((advantage) => (
-        <Table>
+    return advantages.map((advantage, index) => (
+        <Table key={index}>
             <TableBody>
                 <AdvantageRow key={advantage.name} advantage={advantage} />
             </TableBody>
