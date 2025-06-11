e173728d1c8ace28800812632f1cfcabd27d350b
Salavat Nursafin <krowlexing@gmail.com>
2025-06-11 15:28:01 +0400

    update used env variables in pixel checker

--- pixel_checker/README.md
+++ pixel_checker/README.md
@@ -4,5 +4,5 @@ Service for checking pixel installation
 
 ## Required environment variables
 
-- ORIGIN_URL - base url of main allsource service (https://example.com:8080/)
+- API_BASE_URL - base url of main allsource service (https://example.com:8080/)
 - SECRET_PIXEL_KEY - secret value used to confirm your identity, it must match the one in allsource service
--- pixel_checker/tasks.py
+++ pixel_checker/tasks.py
@@ -1,5 +1,3 @@
-import os
-import time
 import logging
 from typing import Optional
 
@@ -7,20 +5,19 @@ from dotenv import load_dotenv
 import httpx
 
 from schemas import DomainsListResponse
+from utils import get_env, get_http_client
 
 load_dotenv()
+
 logger = logging.getLogger(__name__)
 
-API_BASE_URL = os.getenv("ORIGIN_URL")
-SECRET_PIXEL_KEY   = os.getenv("SECRET_PIXEL_KEY")
+SECRET_PIXEL_KEY = get_env("SECRET_PIXEL_KEY")
 
-GET_DOMAINS_URL = f"{API_BASE_URL}/api/install-pixel/verified_domains"
-CHECK_PIXEL_INSTALLATION_URL = f"{API_BASE_URL}/external_api/install-pixel/check-pixel-installed"
 
 async def fetch_domains_with_secret() -> Optional[DomainsListResponse]:
-    async with httpx.AsyncClient() as client:
+    async with get_http_client() as client:
         try:
-            response = await client.get(GET_DOMAINS_URL, params={"secret_key": SECRET_PIXEL_KEY})
+            response = await client.get("/api/install-pixel/verified_domains", params={"secret_key": SECRET_PIXEL_KEY})
             response.raise_for_status()
             data = response.json()
 
@@ -38,8 +35,8 @@ async def fetch_domains_with_secret() -> Optional[DomainsListResponse]:
 
 
 async def fetch_external_data(domain: str) -> None:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(CHECK_PIXEL_INSTALLATION_URL, params={"domain": domain})
+    async with get_http_client() as client:
+        response = await client.get("/external_api/install-pixel/check-pixel-installed", params={"domain": domain})
         if response.status_code == 200:
             logger.info(f"Successfully fetched pixel installation status for domain: {domain}")
         else:
