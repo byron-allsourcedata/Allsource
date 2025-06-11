cdde4b91cc5bc8c93598a65ab90c25c7cc544b22
Salavat Nursafin <krowlexing@gmail.com>
2025-06-11 19:35:00 +0400

    refactor pixel.js conditions

--- pixel_checker/main.py
+++ pixel_checker/main.py
@@ -44,20 +44,19 @@ async def read_item(
 
         domain_to_check = get_domain_from_headers(referer, origin)
 
-        if domain_to_check:
-            if domain_to_check not in valid_domain_cache:
-                logger.info(f"Domain {domain_to_check} not found in cache, fetching external data...")
-                background_tasks.add_task(
+        if domain_to_check and domain_to_check not in valid_domain_cache:
+            logger.info(f"Domain {domain_to_check} not found in cache, fetching external data...")
+            background_tasks.add_task(
                     fetch_external_data, PixelInstallationRequest(
                         pixelClientId=pixel_client_id,
                         url=domain_to_check,
                         need_reload_page=need_reload_page
                     )
                 )
-                valid_domain_cache.add(domain_to_check)
-                logger.info(f"Added {domain_to_check} to referer_cache")
-            else:
-                logger.info(f"Domain {domain_to_check} already in cache, skipping external request")
+            valid_domain_cache.add(domain_to_check)
+            logger.info(f"Added {domain_to_check} to referer_cache")
+        elif domain_to_check in valid_domain_cache:
+            logger.info(f"Domain {domain_to_check} already in cache, skipping external request")
 
         js_content = f"let refererCache = {list(valid_domain_cache)};"
         return PlainTextResponse(content=js_content, media_type="application/javascript")
