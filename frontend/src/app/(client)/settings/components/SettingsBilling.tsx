ce8b7f37f6eb0e55ff5f8dd9a421be8205e5f068
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-06-17 23:11:26 -0700

    remove logging

--- frontend/src/app/(client)/settings/components/SettingsBilling.tsx
+++ frontend/src/app/(client)/settings/components/SettingsBilling.tsx
@@ -114,7 +114,6 @@ export const SettingsBilling: React.FC = () => {
 		try {
 			setIsLoading(true);
 			const response = await axiosInterceptorInstance.get("/settings/billing");
-			console.log(response.data);
 			if (response.data.status == "hide") {
 				setHide(true);
 			} else {
