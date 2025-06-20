baef8a6e7b0ce9fa17418adb6f82211d8cc723f0
donkyHOT <slava.kurenkov@bk.ru>
2025-06-20 17:46:52 +0400

    fix-audience-data-sync-filler

--- backend/bin/audience_data_sync_filler.py
+++ backend/bin/audience_data_sync_filler.py
@@ -4,7 +4,6 @@ import os
 import sys
 
 
-
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 sys.path.append(parent_dir)
