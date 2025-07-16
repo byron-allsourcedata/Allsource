7e1d4d74967453fde03570879827d72666c6a6d0
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-07-16 06:16:55 -0700

    fix without validations

--- backend/bin/audience_smarts_filler.py
+++ backend/bin/audience_smarts_filler.py
@@ -76,7 +76,7 @@ async def aud_smarts_reader(
         data_sources = data.get("data_sources")
         active_segment = data.get("active_segment")
         need_validate = data.get("need_validate")
-        validation_params = data.get("validation_params")
+        validation_params = data.get("validation_params") or {}
 
         priority_raw = audience_settings_persistence.get_validation_priority()
         priority_list = priority_raw.split(",")
@@ -174,14 +174,17 @@ async def aud_smarts_reader(
 
                 logging.info(f"len(persons), {len(persons)}")
 
-                sorted_persons = audience_smarts_service.sorted_enrichment_users_for_validation(
-                    persons, order_by_clause
-                )
+                if order_by_clause:
+                    sorted_persons = audience_smarts_service.sorted_enrichment_users_for_validation(
+                        persons, order_by_clause
+                    )
 
-                logging.info(f"len(sorted_persons), {len(sorted_persons)}")
+                    logging.info(f"len(sorted_persons), {len(sorted_persons)}")
 
-                if not sorted_persons:
-                    break
+                    if not sorted_persons:
+                        break
+                else: 
+                    sorted_persons = persons
 
                 logging.info(
                     f"current count {count}, common count {active_segment // SELECTED_ROW_COUNT}"
