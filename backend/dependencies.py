a71eb78e6c90afc758aeff95effd33fea318e1be
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-07-28 02:29:45 -0700

    fix admin acces to any account

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -426,7 +426,10 @@ def check_user_authorization_without_pixel(
         user_id=user.get("id")
     )
 
-    if auth_status == UserAuthorizationStatus.NEED_CONFIRM_EMAIL:
+    if (
+        auth_status == UserAuthorizationStatus.NEED_CONFIRM_EMAIL
+        and not is_admin
+    ):
         raise_forbidden(
             {
                 "status": UserAuthorizationStatus.NEED_CONFIRM_EMAIL.value,
@@ -461,7 +464,7 @@ def check_user_authorization_without_pixel(
         UserAuthorizationStatus.PIXEL_INSTALLATION_NEEDED,
     }
 
-    if auth_status not in allowed_statuses:
+    if auth_status not in allowed_statuses and not is_admin:
         raise_forbidden({"status": auth_status.value})
 
     return user
@@ -492,7 +495,7 @@ def check_user_setting_access(
     Authorization: Annotated[str, Header()],
     user_persistence_service: UserPersistence,
     privacy_policy_service: PrivacyPolicyService,
-    users_auth_service: UsersAuth
+    users_auth_service: UsersAuth,
 ) -> Token:
     user = check_user_authentication(Authorization, user_persistence_service)
     auth_status = get_user_authorization_status(user, users_auth_service)
