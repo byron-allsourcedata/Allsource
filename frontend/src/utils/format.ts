364d1d6c4cd22d5a4a8f741787109b926c8952f5
krowlexing@gmail.com <krowlexing@gmail.com>
2025-08-07 12:31:35 +0400

    add formatBytes()

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,22 @@
+type ParsedBytes = {
+	size: number;
+	units: string;
+};
+
+function parseBytes(bytes: number): { size: number; units: string } {
+	if (bytes === 0) return { size: 0, units: "B" };
+
+	const units = ["B", "KB", "MB", "GB", "TB"];
+	const i = Math.floor(Math.log(bytes) / Math.log(1024));
+	const size = Number.parseFloat((bytes / 1024 ** i).toFixed(2));
+
+	return { size, units: units[i] };
+}
+
+function formatParsedBytes(bytes: ParsedBytes): string {
+	return `${bytes.size} ${bytes.units}`;
+}
+
+export function formatBytes(bytes: number): string {
+	return formatParsedBytes(parseBytes(bytes));
+}
