83dadd4b98e55bee4f8990557fceb8d05e5d76d5
Nikitimba09 <nickit.schatalow@yandex.ru>
2025-09-01 16:14:21 +0300

    fix models(created at column)

--- backend/models/leads_emails_verification.py
+++ backend/models/leads_emails_verification.py
@@ -8,7 +8,6 @@ from sqlalchemy import (
     BigInteger,
     Boolean,
     Sequence,
-    func,
 )
 
 from .base import Base
@@ -28,5 +27,9 @@ class LeadEmailsVerification(Base):
     )
     email = Column(VARCHAR(128), nullable=False)
     is_verify = Column(Boolean, nullable=False)
-    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    created_at = Column(
+        TIMESTAMP,
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc).replace(tzinfo=None),
+    )
     verify_result = Column(VARCHAR(64), nullable=False)
