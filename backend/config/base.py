7074ca128f9796dde7bb50e4207a483cd06d7b13
Nikitimba09 <nickit.schatalow@yandex.ru>
2025-11-01 12:54:59 +0400

    add allowed domains

--- backend/config/base.py
+++ backend/config/base.py
@@ -10,10 +10,14 @@ class Base:
     env = os.getenv("ENV", "dev")
     port = int(os.getenv("PORT", 8000))
     domain = str(os.getenv("DOMAIN"))
-    allowed_origins = [
-        f"http://{domain}",
-        f"https://{domain}",
-    ]
+
+    raw_domains = os.getenv("ALLOWED_DOMAINS", "")
+    domains = [d.strip() for d in raw_domains.split(",") if d.strip()]
+
+    allowed_origins = []
+    for d in domains:
+        allowed_origins.append(f"http://{d}")
+        allowed_origins.append(f"https://{d}")
 
 
 load_dotenv(find_dotenv(f"{Base.env}.envrc"))
