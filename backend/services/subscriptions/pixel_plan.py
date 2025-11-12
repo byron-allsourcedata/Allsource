379f6a6b3fe2c293ab12e08e7ecbbddfa9d22855
Stanislav Zabrodin <stanislav-zabrodin@outlook.com>
2025-11-12 15:56:38 +0400

    return schedule

--- backend/services/subscriptions/pixel_plan.py
+++ backend/services/subscriptions/pixel_plan.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from persistence.plans_persistence import PlansPersistence
 from resolver import injectable
@@ -36,13 +37,21 @@ class PixelPlanService:
 
         price_id = plan.stripe_price_id
 
+        # session_url = self.stripe.create_checkout_session(
+        #     customer_id=customer_id,
+        #     price_id=price_id,
+        #     payment_intent_data={
+        #         "setup_future_usage": "off_session",
+        #     },
+        #     mode="payment",
+        #     metadata={"type": "upgrade_pixel_plan"},
+        #     success_url=StripeConfig.success_url,
+        # )
+
         session_url = self.stripe.create_checkout_session(
             customer_id=customer_id,
             price_id=price_id,
-            payment_intent_data={
-                "setup_future_usage": "off_session",
-            },
-            mode="payment",
+            mode="subscription",
             metadata={"type": "upgrade_pixel_plan"},
             success_url=StripeConfig.success_url,
         )
@@ -60,26 +69,31 @@ class PixelPlanService:
         self.user_subscriptions.move_to_plan(user_id, "pixel")
         standart_monthly_plan = self.plans.get_plan_by_alias("standard_monthly")
 
-        res = self.stripe.create_pixel_plan_subscription_with_one_time_charge(
-            customer_id=customer_id,
-            future_plan_price_id=standart_monthly_plan.stripe_price_id,
-            trial_days=14,
-        )
-
-        if not res.get("success"):
-            logger.error(
-                f"Failed to create pixel flow for user {user_id}: {res.get('error')}"
-            )
-            return
-
-        subscription = res.get("subscription")
-        plan_end = res.get("plan_end")
-        logger.info(
-            f"User {user_id} moved to pixel plan (trial until {plan_end}), subscription {subscription.get('id')}"
-        )
-
-        # self.stripe.create_shedule_payments(
-        #     subscription_id=subscription_id,
+        # res = self.stripe.create_pixel_plan_subscription_with_one_time_charge(
+        #     customer_id=customer_id,
         #     future_plan_price_id=standart_monthly_plan.stripe_price_id,
-        #     current_period_end=plan_end,
+        #     trial_days=14,
         # )
+
+        # if not res.get("success"):
+        #     logger.error(
+        #         f"Failed to create pixel flow for user {user_id}: {res.get('error')}"
+        #     )
+        #     return
+
+        # subscription = res.get("subscription")
+        # plan_end = res.get("plan_end")
+        # logger.info(
+        #     f"User {user_id} moved to pixel plan (trial until {plan_end}), subscription {subscription.get('id')}"
+        # )
+
+        trial_days = 14
+
+        res = self.stripe.create_shedule_payments(
+            subscription_id=subscription_id,
+            future_plan_price_id=standart_monthly_plan.stripe_price_id,
+            current_period_end=int(time.time()) + int(trial_days) * 24 * 3600,
+        )
+
+        if hasattr(res, "error"):
+            print(res.get("error"))
